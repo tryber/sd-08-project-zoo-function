705ed7439a14786bbe38b290815a963d719e9b18
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2020-12-04 18:35:07 -0300

    req 5

--- src/zoo.js
+++ src/zoo.js
@@ -36,12 +36,14 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  // seu código aqui
+  const retorno = data.employees.some(element => element.managers.includes(id));
+  return retorno
 }
+    isManager('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1');
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  const retorno = data.employees.push(id, firstName, lastName, managers, responsibleFor);
-  return retorno;
+  // const retorno = data.employees.push(id, firstName, lastName, managers, responsibleFor);
+  // return retorno;
   // seu código aqui
 }
 
