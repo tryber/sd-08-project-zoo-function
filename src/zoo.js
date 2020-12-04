00b238a11e05f720b1ceecdd0e74ca7a8c18e151
liisaambrosen <liisa.ambrosen@gmail.com>
2020-12-04 19:17:23 -0300

    codeclimate

--- src/zoo.js
+++ src/zoo.js
@@ -36,8 +36,8 @@ function employeeByName(employeeName) {
 const createEmployee = (personalInfo, associatedWith) =>
   Object.assign({}, personalInfo, associatedWith);
 
-const isManager = id => data.employees
-  .some((person) => person.id === id && person.managers.length === 1);
+const isManager = id =>
+data.employees.some(person => person.id === id && person.managers.length === 1);
 
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
