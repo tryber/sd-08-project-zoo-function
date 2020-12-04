a052737da4af8b53c1f6ee2495222786050cf2a8
fabio-infotech <fabio35547@gmail.com>
2020-12-04 17:11:17 -0300

    requisito 6

--- src/zoo.js
+++ src/zoo.js
@@ -44,8 +44,8 @@ function isManager(id) {
   return employees.some(employee => employee.managers.some(managerId => managerId === id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
 
 function animalCount(species) {
