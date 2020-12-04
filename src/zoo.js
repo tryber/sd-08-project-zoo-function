6c1e5d6bf16e50f78146ef9fc86f8d8d870a999d
vicoliveir <victorhugoqxd@gmail.com>
2020-12-04 17:16:22 -0300

    initializing project, requirement 1 test

--- src/zoo.js
+++ src/zoo.js
@@ -11,8 +11,11 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
+function animalsByIds(...ids) {
+  if (ids.length === 0) {
+    return [];
+  }
+  return data.animals.filter(animal => ids.includes(animal.id));
 }
 
 function animalsOlderThan(animal, age) {
