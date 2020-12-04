463fe5af5abe448cf2e362610bee2061c3d9fbcc
momentts <marcosd3wow@gmail.com>
2020-12-04 15:32:34 -0300

    testando a questao 1 novamente2

--- src/zoo.js
+++ src/zoo.js
@@ -12,8 +12,8 @@ eslint no-unused-vars: [
 const data = require('./data');
 
 function animalsByIds(...ids) {
- return data.animals.filter(animal => ids.find(id => id === animal.id));
-};
+  return data.animals.filter(animal => ids.find(id => id === animal.id));
+}
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
