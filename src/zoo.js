34bea5c8e20e4349903ae081598cd03c483fe00c
Mateus Coury <mateuscoury@gmail.com>
2020-12-04 18:55:17 -0300

    exercicio4

--- src/zoo.js
+++ src/zoo.js
@@ -33,6 +33,7 @@ function employeeByName(employeeName) {
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
+  return ({ ...personalInfo, ...associatedWith })
 }
 
 function isManager(id) {
