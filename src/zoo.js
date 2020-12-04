67375b90535f934ed2d67ca7a2f0c20f92d3bd76
Max <gmaxgomes@gmail.com>
2020-12-04 18:32:21 -0300

    req 3

--- src/zoo.js
+++ src/zoo.js
@@ -9,7 +9,7 @@ eslint no-unused-vars: [
 ]
 */
 
-const { animals } = require('./data');
+const { animals, employees } = require('./data');
 const data = require('./data');
 
 function animalsByIds(...ids) {
@@ -26,7 +26,13 @@ function animalsOlderThan(animal, age) {
 
 function employeeByName(employeeName) {
   // seu código aqui
+  if (typeof employeeName === 'undefined') {
+    return {};
+  } else {
+    return employees.find((employee) => employee.firstName === employeeName || employee.lastName === employeeName);
+  }
 }
+console.log(employeeByName('Emery'));
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
