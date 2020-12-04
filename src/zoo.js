8ffa70163a9f914a0dfa1b40aeb0b79a40272c7b
fabio-infotech <fabio35547@gmail.com>
2020-12-04 17:55:51 -0300

    requisito 7 - code climate 2

--- src/zoo.js
+++ src/zoo.js
@@ -50,10 +50,9 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
 
 function animalCount(species) {
   if (!species) {
-    return (animals.reduce((currAnimal, prevAnimal) => 
-    Object.assign(currAnimal, { [prevAnimal.name]: prevAnimal.residents.length }), {}));
+    return (animals.reduce((currAnimal, prevAnimal) =>
+      Object.assign(currAnimal, { [prevAnimal.name]: prevAnimal.residents.length }), {}));
   }
-  
   return animals.find(({ name }) => name === species).residents.length;
 }
 
