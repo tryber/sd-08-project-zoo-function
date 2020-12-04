406316e98f1400762aa173c71ad5135ac8714c51
emassaki <massakihirayama@gmail.com>
2020-12-04 16:06:56 -0300

    Requisito 1 - encontrar os animais pelo ID

--- src/zoo.js
+++ src/zoo.js
@@ -9,10 +9,11 @@ eslint no-unused-vars: [
 ]
 */
 
-const data = require('./data');
+const data = require("./data");
+const { animals, employees } = data;
 
-function animalsByIds(ids) {
-  // seu código aqui
+function animalsByIds(...ids) {
+  return animals.filter((animal) => ids.includes(animal.id))
 }
 
 function animalsOlderThan(animal, age) {
