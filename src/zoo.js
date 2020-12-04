69a140f1c427e76239ee8d9e33f1e32771e7cf2c
Igor Nascimento <igorln96@gmail.com>
2020-12-04 17:32:43 -0300

    req3 - employeeByName

--- src/zoo.js
+++ src/zoo.js
@@ -22,7 +22,11 @@ function animalsOlderThan(animal, age) {
 }
 
 function employeeByName(employeeName) {
-  // seu código aqui
+  if (typeof employeeName !== 'string') {
+    return {};
+  }
+  return data.employees.find(employee =>
+    employee.firstName === employeeName || employee.lastName === employeeName);
 }
 
 function createEmployee(personalInfo, associatedWith) {
