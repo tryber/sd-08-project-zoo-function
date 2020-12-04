6f3ac28dd22cc87c488ecb33659f328d96062ffb
Bruno Pedrosa <bgpedrosas@gmail.com>
2020-12-04 15:02:14 -0300

    req 3

--- src/zoo.js
+++ src/zoo.js
@@ -22,7 +22,9 @@ function animalsOlderThan(animal, age) {
 }
 
 function employeeByName(employeeName) {
-  // seu código aqui
+  return employeeName ? data.employees
+    .find(
+      (employee) => employee.firstName === employeeName || employee.lastName === employeeName) : {};
 }
 
 function createEmployee(personalInfo, associatedWith) {
