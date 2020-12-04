77ceb78b299e92918c3f89dca6cf882b4394ae94
momentts <marcosd3wow@gmail.com>
2020-12-04 16:15:19 -0300

    testando o quesito 2 novamente

--- src/zoo.js
+++ src/zoo.js
@@ -17,7 +17,7 @@ function animalsByIds(...ids) {
 
 function animalsOlderThan(animal, age) {
   return data.animals.find(species => species.name === animal)
-  .residents.every(age1 => age1.age > age)
+  .residents.every(age1 => age1.age > age);
 }
 
 function employeeByName(employeeName) {
