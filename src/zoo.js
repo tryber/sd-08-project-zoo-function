2975948921ca4379dd9aa0d2a90ba4443f09660a
Fernando Caique Soares <fndcaique@gmail.com>
2020-12-04 13:51:28 -0300

    testizaum

--- src/zoo.js
+++ src/zoo.js
@@ -13,6 +13,9 @@ const data = require('./data');
 
 function animalsByIds(ids) {
   // seu código aqui
+  if (ids === undefined) {
+    return [];
+  }
   return [{
     id: '0938aa23-f153-4937-9f88-4858b24d6bce',
     name: 'lions',
