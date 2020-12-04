61085ad8eb5096a19c46bb635dca3477d8a6ba7a
Ruan Miguel <ruanmiguelps@gmail.com>
2020-12-04 15:50:36 -0300

    codeclimate

--- src/zoo.js
+++ src/zoo.js
@@ -19,7 +19,6 @@ function animalsByIds(...ids) {
   ids.forEach(ids => allids.push(animals.find(element => element.id === ids)));
   return allids;
 }
-console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce'))
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
