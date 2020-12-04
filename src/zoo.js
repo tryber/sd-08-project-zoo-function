9bc2145d6ff2d3cdce8ebef0512abf50be336cbc
Lucas Gama <lucasbg@protonmail.com>
2020-12-04 16:45:49 -0300

    Requisito 5: Função que verifica se o id passado é de um gerente

--- src/zoo.js
+++ src/zoo.js
@@ -32,7 +32,7 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some(({ managers }) => managers.includes(id));
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
