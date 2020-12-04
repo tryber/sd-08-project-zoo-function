e68ec172c18a86ba20ade189337b3cea84f46442
Rodrigo de Souza <rdesouza2008@gmail.com>
2020-12-04 15:11:44 -0300

    requisito 6

--- src/zoo.js
+++ src/zoo.js
@@ -41,8 +41,14 @@ function isManager(id) {
   return employees.some(({ managers }) => managers.some(index => index === id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  employees.push({
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  });
 }
 
 function animalCount(species) {
