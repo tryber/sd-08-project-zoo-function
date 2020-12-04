23edf077b68b3a34c6ba9d659a315de2d00db2ec
Rodrigo de Souza <rdesouza2008@gmail.com>
2020-12-04 12:53:13 -0300

    requisito 5

--- src/zoo.js
+++ src/zoo.js
@@ -36,7 +36,7 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some(({managers}) => managers.some((index) => index === id))
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
