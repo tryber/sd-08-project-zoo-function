5b76bb495ecbcad74bb869dc76abb12b5f7d2021
ThamirysSantos <thamirysgoncalves.prog@gmail.com>
2020-12-04 18:50:19 -0300

    realizado requisito 1 - correçao code-climate

--- src/zoo.js
+++ src/zoo.js
@@ -17,14 +17,11 @@ function animalsByIds(...ids) {
   const animalsArray = [];
   if (ids !== undefined) {
     ids.forEach(element => {
-      animalsArray.push(animals.find((animals) => (animals.id === element)));
+      animalsArray.push(animals.find(animals => (animals.id === element)));
     });
   }
-   
   return animalsArray;
 }
-
-console.log(animalsByIds("0938aa23-f153-4937-9f88-4858b24d6bce","e8481c1d-42ea-4610-8e11-1752cfc05a46"));
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
