af13eea5a85c9bfdfcbe62043fe3052ff8008824
jnpefo <jnpefo@gmail.com>
2020-12-04 17:36:12 -0300

    Req. 2, tentativa 1

--- src/zoo.js
+++ src/zoo.js
@@ -18,9 +18,13 @@ function animalsByIds(...ids) {
   return data.animals.filter(animal => ids.includes(animal.id));
 };
 
+// console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
+
 function animalsOlderThan(animal, age) {
-  // seu código aqui
+  return data.animals.find(item => item.name === animal).residents.every(item => item.age > age);
 }
+
+// console.log(animalsOlderThan('penguins', 10));
 
 function employeeByName(employeeName) {
   // seu código aqui
