b291fba12857efacf6d78ba9c6815ef3be590303
VitorLimaRios <vitorlimarios999@gmail.com>
2020-12-04 14:48:43 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/zoo.js
+++ src/zoo.js
@@ -9,10 +9,11 @@ eslint no-unused-vars: [
 ]
 */
 
-const data = require('./data');
+const data = require("./data");
 
 function animalsByIds(ids) {
   // seu código aqui
+  return ids;
 }
 
 function animalsOlderThan(animal, age) {
