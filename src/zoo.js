cbc662ceb5f7f77e3761f2bfb1304ff85db671dd
MEianelli <filipelouzano@gmail.com>
2020-12-04 18:09:10 -0300

    Codeclimate Round 3

--- src/zoo.js
+++ src/zoo.js
@@ -16,7 +16,7 @@ const { hours } = require('./data');
 function animalsByIds(...ids) {
   if (ids.length === 0) return [];
   return animals.filter((e) => {
-    ids.includes(e.id);
+    return ids.includes(e.id);
   });
 }
 
