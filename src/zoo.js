922165484403befebc284696695db077e166b429
Bruno Wedley <brunowedley27@gmail.com>
2020-12-04 15:39:56 -0300

    :beetle: adequação ao codeclimate req1

--- src/zoo.js
+++ src/zoo.js
@@ -15,7 +15,7 @@ const data = require('./data');
 function animalsByIds(...ids) {
   const animalArr = [];
   ids.forEach((animal) => {
-    animalArr.push(animals.find(animalID => animalID.id === animal)
+    animalArr.push(animals.find((animalID) => animalID.id === animal)
     );
   });
   return animalArr;
