8cb13a996ffb40e36f8dd41799706adfa89e85cc
Rafael <rafaelmaltez89@gmail.com>
2020-12-04 16:59:19 -0300

    Requisite 03 employeeByName codeclimate correction

--- src/zoo.js
+++ src/zoo.js
@@ -30,7 +30,8 @@ function employeeByName(employeeName) {
   if (!employeeName) {
     return {};
   }
-  return employees.find(employee => employee.firstName === employeeName || employee.lastName === employeeName)
+  return employees
+    .find(employee => employee.firstName === employeeName || employee.lastName === employeeName);
 }
 
 function createEmployee(personalInfo, associatedWith) {
