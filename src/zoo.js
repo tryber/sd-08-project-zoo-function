4375316e65fd76117531211682204ba7eb537cfc
Rafael <rafaelmaltez89@gmail.com>
2020-12-04 16:22:44 -0300

    Requisite 01, animalsByIds

--- src/zoo.js
+++ src/zoo.js
@@ -9,11 +9,16 @@ eslint no-unused-vars: [
 ]
 */
 
+const { animals } = require('./data');
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
-}
+function animalsByIds(...ids) {
+  if(ids.length === 0) {
+    return [];
+  }
+  return ids.map((id) => animals.find((animal) => animal.id === id));
+  //feito com dica do Lauro no plantão.
+};
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
