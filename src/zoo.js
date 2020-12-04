2c3a3ae53d4593f04f2388dc4f040d7f151bba37
Daniel <danieldsrd@gmail.com>
2020-12-04 20:24:50 -0300

    Requisito 8

--- src/zoo.js
+++ src/zoo.js
@@ -11,6 +11,7 @@ eslint no-unused-vars: [
 
 const { animals } = require('./data');
 const { employees } = require('./data');
+const { prices } = require('./data');
 const data = require('./data');
 
 function animalsByIds(...ids) {
@@ -83,7 +84,11 @@ function animalCount(species) {
 }
 
 function entryCalculator(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  return Object.keys(entrants).reduce(
+    (acc, value) => acc + (prices[value] * entrants[value]), 0);
 }
 
 function animalMap(options) {
