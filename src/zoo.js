db08d918b1aee61f49b83f94df3a5f368b7205e0
Rafael <rafaelmaltez89@gmail.com>
2020-12-04 18:08:07 -0300

    Requisite 06 addEmployee

--- src/zoo.js
+++ src/zoo.js
@@ -46,9 +46,14 @@ function isManager(id) {
     .some(element => element.includes(id));
 }
 
-
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  employees.push({
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  });
 }
 
 function animalCount(species) {
