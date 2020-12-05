a8aac5152b8e86e8ecd9f0b5748bc7a13589a4e7
mateusvictor7 <mateussantiago.victor@gmail.com>
2020-12-05 15:25:23 -0300

    Requisito 3

--- src/zoo.js
+++ src/zoo.js
@@ -9,7 +9,7 @@ eslint no-unused-vars: [
 ]
 */
 
-const { animals } = require('./data');
+const { animals, employees } = require('./data');
 const data = require('./data');
 
 function animalsByIds(...ids) {
@@ -22,7 +22,10 @@ function animalsOlderThan(animalName, age) {
 }
 
 function employeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) return {};
+  return employees.find(employee => (
+    employee.firstName === employeeName || employee.lastName === employeeName
+  ));
 }
 
 function createEmployee(personalInfo, associatedWith) {
