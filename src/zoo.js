b83ad43115eb4135b35ad7467093ab4d96ebd82a
Hugo Andrade Braga <hugobrr@gmail.com>
2020-12-05 08:00:12 -0300

    3-requisito concluido com zero erros no cc local corrigindo arquivo do CC  - 15%

--- src/zoo.js
+++ src/zoo.js
@@ -55,6 +55,17 @@ function animalsOlderThan(animal, age) {
 
 function employeeByName(employeeName) {
   // seu código aqui
+  // Esta função é responsável pela busca das pessoas colaboradoras através do
+  // primeiro ou do último nome delas
+  // O que será avaliado
+  // Sem parâmetros, retorna um objeto vazio
+  // Quando provido o primeiro nome do funcionário, retorna o objeto do funcionário
+  // Quando provido o último nome do funcionário, retorna o objeto do funcionário
+  if (typeof employeeName === 'undefined') { return {}; }
+  return data.employees.find((employee) => {
+    const result = employee.firstName === employeeName || employee.lastName === employeeName;
+    return result;
+  });
 }
 
 function createEmployee(personalInfo, associatedWith) {
