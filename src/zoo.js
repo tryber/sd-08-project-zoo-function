04db00a753a43b742b205e3361179be4a710548d
Uilton Rocha <itoclick@gmail.com>
2020-12-05 01:56:22 -0300

    entregavel1

--- src/zoo.js
+++ src/zoo.js
@@ -9,11 +9,14 @@ eslint no-unused-vars: [
 ]
 */
 
+const { animals } = require('./data');
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
-}
+function animalsByIds(...ids) { 
+  if (!ids) return [];
+  return animals.filter((actua)=> actua.id === ids[0] ||actua.id === ids[1])
+};
+
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
