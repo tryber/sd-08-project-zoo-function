4d3c2a39d606a0253c48db9e51adb7c797255222
enicio <jessandro42@gmail.com>
2020-12-05 16:55:37 -0300

    Requisito 5 refatorado

--- src/zoo.js
+++ src/zoo.js
@@ -13,34 +13,38 @@ const { animals, employees } = require('./data');
 const data = require('./data');
 
 function animalsByIds(...ids) {
-  const idetn = ids;
-  const animalById = animals.filter((animal, index) => animal.id === idetn[index]);
+  const id = ids;
+  const animalById = animals.filter((animal, index) => animal.id === id[index]);
   return animalById;
 }
 
 function animalsOlderThan(animal, age) {
-  const ops = animal;
-  const whatAnimal = animals.filter(animales => animales.name === ops);
-  const kombi = whatAnimal[0].residents.map(oque => oque.age);
-  let sum = 0;
-  kombi.forEach((item) => {
-    sum += item;
-  });
-  if (sum / kombi.length > age) {
+  const animalFound = animals.filter(eachAnimal => eachAnimal.name === animal);
+  const arrayOfAge = animalFound[0].residents.map(resident => resident.age);
+  const sum = arrayAvrg(arrayOfAge);
+  if (sum / arrayOfAge.length > age) {
     return true;
   } return false;
 }
+
+const arrayAvrg = ((array) => {
+  let sum = 0;
+  array.forEach((item) => {
+    sum += item;
+  });
+  return sum;
+});
 
 function employeeByName(employeeName) {
   if (employeeName === undefined) {
     return {};
   }
-  const ops = employees.find(cada => cada.firstName === employeeName);
-  const ops2 = employees.find(cada => cada.lastName === employeeName);
-  if (ops !== undefined) {
-    return ops;
+  const getEmployeeByFirstName = employees.find(employee => employee.firstName === employeeName);
+  const getEmployeeByLastName = employees.find(employee => employee.lastName === employeeName);
+  if (getEmployeeByFirstName !== undefined) {
+    return getEmployeeByFirstName;
   }
-  if (ops2 !== undefined) return ops2;
+  if (getEmployeeByLastName !== undefined) return getEmployeeByLastName;
   return true;
 }
 
@@ -57,14 +61,8 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  const ops = employees.find((gerente) => {
-    if (gerente.managers[0] === id) return gerente;
-    return false;
-  });
-  if (ops !== undefined) {
-    return true;
-  }
-  return false;
+  const manager = employees.map(manager => manager.managers);
+  return manager.some((manageres, index) => manageres[index] === id)
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
