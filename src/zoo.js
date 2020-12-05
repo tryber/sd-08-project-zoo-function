bb06f088072b34c7ec6e0e7c62402b2ab181fc71
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-12-05 00:13:20 -0300

    Feito ate o Req 6, tentativa 1

--- src/zoo.js
+++ src/zoo.js
@@ -9,30 +9,38 @@ eslint no-unused-vars: [
 ]
 */
 
+const { animals } = require('./data');
 const data = require('./data');
 
-function animalsByIds(ids) {
+function animalsByIds(...ids) {
   // seu código aqui
+  return animals.filter(({ id }) => ids.includes(id));
 }
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
-}
+  return animals.find(current => current.name === animal).residents.every(resident => resident.age >= age);
+} 
 
 function employeeByName(employeeName) {
   // seu código aqui
+  if (typeof employeeName !== 'string' || employeeName.length === 0) return {};
+  return data.employees.find(element => element.firstName === employeeName) || data.employees.find(element => element.lastName === employeeName);
 }
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
+  return ({...personalInfo, ...associatedWith});
 }
 
 function isManager(id) {
   // seu código aqui
+  return data.employees.some(element => element.managers.includes(id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
   // seu código aqui
+  return data.employees.push({ id, firstName, lastName, managers, responsibleFor});
 }
 
 function animalCount(species) {
