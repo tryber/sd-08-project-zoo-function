d0f78e92452fb6144f02e4777a6c827818546ccc
wend3ll-souza <wend3llsouza@gmail.com>
2020-12-05 11:58:30 -0300

    :up: AnimailsByID pronto, requisito 1

--- src/zoo.js
+++ src/zoo.js
@@ -9,18 +9,20 @@ eslint no-unused-vars: [
 ]
 */
 
+const { animals } = require('./data');
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
+function animalsByIds(...ids) {
+  return animals.filter(element => ids.includes(element.id))
 }
 
+
 function animalsOlderThan(animal, age) {
-  // seu código aqui
+
 }
 
 function employeeByName(employeeName) {
-  // seu código aqui
+  
 }
 
 function createEmployee(personalInfo, associatedWith) {
