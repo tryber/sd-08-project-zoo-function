20a918a96826ff9567ed7ee8f671baa7826d59f3
Celso Oliva Aparecido <celso.oliva.aparecido@gmail.com>
2020-12-05 02:06:45 -0300

    req9: como eu pensei na implementacao desse requisito mais desafiador com tipos bem característicos e aninhados

--- src/zoo.js
+++ src/zoo.js
@@ -79,9 +79,69 @@ function entryCalculator(entrants) {
   return total;
 }
 
-function animalMap(options) {
-  // seu código aqui
+function defaultMap(options = { includeNames: false, sorted: false, sex: undefined }) {
+  const output = animals.reduce((objMapAnimals, { name, location }) => {
+    if (objMapAnimals[location] === undefined) objMapAnimals[location] = [];
+    objMapAnimals[location].push(name);
+    return objMapAnimals;
+  }, {});
+  return output;
 }
+
+function includeNamesMap(options = { includeNames: false, sorted: false, sex: undefined }) {
+  const output = animals.reduce((objMapAnimals, { name, location, residents }) => {
+    if (objMapAnimals[location] === undefined) objMapAnimals[location] = [];
+    const newAnimal = {};
+    newAnimal[name] = residents
+      .map(resident => resident.name);
+    objMapAnimals[location].push(newAnimal);
+    return objMapAnimals;
+  }, {});
+  return output;
+}
+
+function sortedTrueMap() {
+  const output = animals.reduce((objMapAnimals, { name, location, residents }) => {
+    if (objMapAnimals[location] === undefined) objMapAnimals[location] = [];
+    const newAnimal = {};
+    newAnimal[name] = residents
+      .map(resident => resident.name)
+      .sort();
+    objMapAnimals[location].push(newAnimal);
+    return objMapAnimals;
+  }, {});
+  return output;
+}
+
+function animalSexMap(options = { includeNames: false, sorted: false, sex: undefined }) {
+  const output = animals.reduce((objMapAnimals, { name, location, residents }) => {
+    if (objMapAnimals[location] === undefined) objMapAnimals[location] = [];
+    const newAnimal = {};
+    newAnimal[name] = residents
+      .filter(resident => resident.sex === options.sex)
+      .map(resident => resident.name);
+    objMapAnimals[location].push(newAnimal);
+    return objMapAnimals;
+  }, {});
+  return output;
+}
+
+function animalMap(options) {
+  const output = animals.reduce((objMapAnimals, { name, location, residents }) => {
+    if (objMapAnimals[location] === undefined) objMapAnimals[location] = [];
+    // seu código aqui
+    // includeNames -> includeNames: true
+    const newAnimal = {};
+    newAnimal[name] = residents
+      .filter(resident => resident.sex === options.sex) // -> sex: male || female
+      .map(resident => resident.name)
+      .sort(); // -> sorted: true
+    objMapAnimals[location].push(newAnimal);
+    return objMapAnimals;
+  }, {});
+  return output;
+}
+console.log(animalMap({sex: 'male'}));
 
 function schedule(dayName) {
   // seu código aqui
