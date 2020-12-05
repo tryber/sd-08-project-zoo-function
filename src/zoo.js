495497ee6030e4c2105a969b89e3cb619757a45d
victor Farias <victorfarias.tv@gmail.com>
2020-12-05 15:25:39 -0300

    Segundo requisito CodeCLimate

--- src/zoo.js
+++ src/zoo.js
@@ -20,7 +20,8 @@ function animalsByIds(...ids) {
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
-  return animals.find(animalName => animalName.name === animal).residents.every(animalAge => animalAge.age >= age);
+  return animals.find(animalName => animalName.name === animal)
+    .residents.every(animalAge => animalAge.age >= age);
 }
 
 function employeeByName(employeeName) {
