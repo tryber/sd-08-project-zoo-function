477ec337ff6ae054e3d109f0f56961ca9110e6ba
Douglas Ferreira <dodofp.df@gmail.com>
2020-12-05 19:34:40 -0300

    Requisito 4

--- src/zoo.js
+++ src/zoo.js
@@ -38,6 +38,10 @@ function employeeByName(employeeName) {
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
+  return {
+    ...personalInfo,
+    ...associatedWith,
+  };
 }
 
 function isManager(id) {
