649e49b820a0d7e2a4ffe32fc619226fa52008a1
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2020-12-05 16:37:24 -0300

    resolve req6

--- src/zoo.js
+++ src/zoo.js
@@ -39,7 +39,13 @@ function isManager(id) {
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+  return employees.push({
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor
+  });
 }
 
 function animalCount(species) {
