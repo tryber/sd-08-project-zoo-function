d36b84c838608b218a519083a3b36462a8fa9430
Ediberto Oliveira <edibertooliveira@aol.com>
2020-12-05 14:38:14 -0300

    chore: codeclimate

--- src/zoo.js
+++ src/zoo.js
@@ -18,8 +18,7 @@ const animalsOlderThan = (animal, key) =>
 
 const employeeByName = employeeName =>
   employees.find(
-    ({ firstName, lastName }) => firstName === employeeName || lastName === employeeName
-  ) || {};
+    ({ firstName, lastName }) => firstName === employeeName || lastName === employeeName) || {};
 
 const createEmployee = (personalInfo, associatedWith) => ({ ...personalInfo, ...associatedWith });
 
@@ -29,51 +28,52 @@ const isManager = id =>
 const addEmployee = (id, firstName, lastName, managers = [], responsibleFor = []) =>
   employees.push({ id, firstName, lastName, managers, responsibleFor });
 
-const animalCount = species =>
-  species
-    ? animals.find(animal => animal.name === species).residents.length
-    : animals.reduce(
-        (accAnimal, currAnimal) =>
-          Object.assign(accAnimal, { [currAnimal.name]: currAnimal.residents.length }),
-        {}
-      );
+const animalCount = (species) => {
+  if (species) return animals.find(animal => animal.name === species).residents.length;
+  return animals.reduce(
+    (accAnimal, currAnimal) =>
+    Object.assign(accAnimal, { [currAnimal.name]: currAnimal.residents.length }),
+    {});
+};
 
-function entryCalculator(entrants = 0) {
+const entryCalculator = (entrants = 0) => {
   let countAnimals = 0;
   Object.keys(entrants).forEach(key => (countAnimals += prices[key] * entrants[key]));
   return countAnimals;
-}
+};
 
 function animalMap(options) {
   // seu código aqui
 }
 
-function schedule(dayName) {
+const schedule = (dayName) => {
   let week = {};
-  Object.keys(hours).forEach(day => {
+  Object.keys(hours).forEach((day) => {
     const weekDay = !dayName ? day : dayName;
     if (hours[weekDay].open === 0) {
-      week = { ...week, [weekDay]: `CLOSED` };
-    } else
+      week = { ...week, [weekDay]: 'CLOSED' };
+    } else {
       week = {
         ...week,
         [weekDay]: `Open from ${hours[weekDay].open}am until ${hours[weekDay].close - 12}pm`,
       };
+    }
   });
   return week;
-}
-
-const oldestFromFirstSpecies = id => {
-  const animalId = employees.find(({ id: idKey }) => idKey === id).responsibleFor;
-  const { residents } = animals.find(({ id: idKey }) => idKey === animalId[0]);
-  const { name, sex, age } = residents.reduce((accAnimal, currAnimal) =>
-    accAnimal.age > currAnimal.age ? accAnimal : currAnimal
-  );
-  return [name, sex, age];
 };
 
-const increasePrices = percentage => {
-  const discount = percentage / 100 + 1;
+const oldestFromFirstSpecies = (id) => {
+  const animalId = employees.find(({ id: idKey }) => idKey === id).responsibleFor;
+  const { residents } = animals.find(({ id: idKey }) => idKey === animalId[0]);
+  const firstAnimal = residents.reduce((accAnimal, currAnimal) => {
+    if (accAnimal.age > currAnimal.age) return accAnimal;
+    return currAnimal;
+  });
+  return Object.values(firstAnimal);
+};
+
+const increasePrices = (percentage) => {
+  const discount = ((percentage / 100) + 1);
   prices.Adult = Math.round(prices.Adult * 100 * discount) / 100;
   prices.Child = Math.round(prices.Child * 100 * discount) / 100;
   prices.Senior = Math.round(prices.Senior * 100 * discount) / 100;
