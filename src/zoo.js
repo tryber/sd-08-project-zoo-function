f6602158dac54dda170dddf573688610729bc614
Nícolas Tozato <ntozato9@gmail.com>
2020-12-06 16:26:23 -0300

    codeclimate

--- src/zoo.js
+++ src/zoo.js
@@ -17,7 +17,7 @@ function animalsByIds(...ids) {
   if (ids === undefined) {
     return [];
   }
-  return animals.filter((animal) => animal.id === ids);
+  return animals.filter(animal => animal.id === ids);
 }
 
 function animalsOlderThan(animal, age) {
