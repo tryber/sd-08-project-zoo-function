10207a7bf73b6a580c6f96750788976e40223aef
Victor Breno <victor.brns2@gmail.com>
2020-12-06 02:29:01 -0300

    requisito 6

--- src/zoo.js
+++ src/zoo.js
@@ -72,24 +72,6 @@ function createEmployee(personalInfo, associatedWith) {
   return newEmployee;
 }
 
-const personalInfo = {
-  id: '7ed1c9bb-8570-44f6-b718-0666b869573a',
-  firstName: 'John',
-  lastName: 'Doe',
-};
-
-const associatedWith = {
-  managers: [
-    'd5b83cb3-a451-49e2-ac45-ff3f54fbe7e1',
-    '9e7d4524-363c-416a-8759-8aa7e50c0992'],
-  responsibleFor: [
-    '0938aa23-f153-4937-9f88-4858b24d6bce',
-    '89be95b3-47e4-4c5b-b687-1fabf2afa274',
-    'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'] };
-
-createEmployee(personalInfo, associatedWith);
-
-
 function isManager(id) {
   let cont = 0;
   let retorno;
@@ -108,7 +90,21 @@ function isManager(id) {
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+  const secondNewEmployee = {};
+  secondNewEmployee.id = id;
+  secondNewEmployee.firstName = firstName;
+  secondNewEmployee.lastName = lastName;
+  if (managers === undefined) {
+    secondNewEmployee.managers = [];
+  } else {
+    secondNewEmployee.managers = managers;
+  }
+  if (responsibleFor === undefined) {
+    secondNewEmployee.responsibleFor = [];
+  } else {
+    secondNewEmployee.responsibleFor = responsibleFor;
+  }
+  employees.push(secondNewEmployee);
 }
 
 function animalCount(species) {
