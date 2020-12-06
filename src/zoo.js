add071db537b133cf86068a194d7a9f7ff8affce
Ruben Albertassi <albertassi88@gmail.com>
2020-12-05 21:22:27 -0300

    Alteracoes no codigo para o teste

--- src/zoo.js
+++ src/zoo.js
@@ -11,12 +11,12 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(...ids) { 
+function animalsByIds(...ids) {
   return data.animals.filter(animal => ids.includes(animal.id));
 }
 
 function animalsOlderThan(animal, age) {
-  return anim = data.animals.find(a => a.name === animal).residents.every(a => a.age > age);  
+  return anim = data.animals.find(a => a.name === animal).residents.every(a => a.age > age);
 }
 
 function employeeByName(employeeName) {
@@ -35,13 +35,13 @@ function isManager(id) {
 }
 
 function addEmployee(id, firstName, lastName, managers=[], responsibleFor=[]) {
-  data.employees.push({ id, firstName, lastName, managers, responsibleFor });
+  data.employees.push({id, firstName, lastName, managers, responsibleFor});
 }
 
 function animalCount(species) {
   const objectAnimal = data.animals.reduce((cont, animal) => {cont[animal.name] = animal.residents.length;
     return cont;
-  }, {});
+  },{});
   if (species === undefined) {
     return objectAnimal;
   }
@@ -49,75 +49,40 @@ function animalCount(species) {
 }
 
 function entryCalculator(entrants) {
-  const people = data.prices;   
+  const people = data.prices;
   if (entrants === undefined || entrants.length === 0) {
     return 0;
   }
-  return Object.entries(entrants).reduce((cont, [key, valor]) => cont + people[key] * valor, 0);  
+  return Object.entries(entrants).reduce((cont, [key, valor]) => cont + people[key] * valor, 0);
 }
-
-
-
-
-
-
-
-
-
 
 function animalMap(options) {
   // seu código aqui
 }
 
-
-
-
-
-
 function schedule(dayName) {
-  const { hours } = data;   
+  const { hours } = data;
   const resut = Object.entries(hours).reduce((cont, [key, value]) => {
-      cont[key] = value.open === 0 & value.close === 0 ? `CLOSED` : `Open from ${value.open}am until ${value.close - 12}pm`
-      return cont;
-  }, {});    
+    cont[key] = value.open === 0 & value.close === 0 ? `CLOSED` : `Open from ${value.open}am until ${value.close - 12}pm`
+    return cont;
+  },{});
   if (dayName === undefined) {
     return resut;
-  }  
+  }
   return { [dayName] : resut[dayName] }
 }
 
-
-
-
-
-
-
-
-
-
-
-
 function oldestFromFirstSpecies(id) {
-  
-  
+
 }
-
-
-
-
-
-
 
 function increasePrices(percentage) {
-  // seu código aqui
+  const porcentagem = 1 + (percentage / 100);
+  return Object.entries(data.prices).reduce((cont, [key, value]) => {
+    cont[key] = Math.round(value * porcentagem * 100) / 100;
+    return cont;
+  },{});
 }
-
-
-
-
-
-
-
 
 function employeeCoverage(idOrName) {
   // seu código aqui
