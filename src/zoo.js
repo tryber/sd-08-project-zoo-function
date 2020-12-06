926807c75f1b0d86ab81183be04fa7f47ee02ebf
Doug-Fernando <doug.fernando95@gmail.com>
2020-12-06 20:53:53 -0300

    Corrigindo erros codeclimate

--- src/zoo.js
+++ src/zoo.js
@@ -17,24 +17,17 @@ function animalsByIds(...ids) {
     return ids;
   }
 
-  let animalsResult = [];
+  const animalsResult = [];
   ids.forEach((animalId) => {
-    data.animals.forEach((animal) => {
+    animals.forEach((animal) => {
       if (animal.id === animalId) {
         animalsResult.push(animal);
       }
-    })
-  })
+    });
+  });
 
   return animalsResult;
-
 }
-
-console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce'))
-
-data.animals.forEach((animal) => {
-  console.log(`1 ${animal.id}`)
-})
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
