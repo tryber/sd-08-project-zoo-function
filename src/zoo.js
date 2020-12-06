3c7d3a30be46a9843586b0df5295327e65ff335c
Giovanna Betti <giovannabetti@gmail.com>
2020-12-06 18:41:28 -0300

    Implementing addEmployee function

--- src/zoo.js
+++ src/zoo.js
@@ -40,8 +40,14 @@ function isManager(id) {
   return employees.some(employee => employee.managers.includes(id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  employees.push({
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  });
 }
 
 function animalCount(species) {
