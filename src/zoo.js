6b147c3291d5da17770fc56d803a91a95f6a1e71
Joao <joaocarlitoneto@gmail.com>
2020-12-05 22:25:53 -0300

    Send Exercise 08 without issues

--- src/zoo.js
+++ src/zoo.js
@@ -1,5 +1,5 @@
 // const data = require('./data');
-const { animals, employees } = require('./data');
+const { animals, employees, prices } = require('./data');
 
 function animalsByIds(...ids) {
   if (!ids) return [];
@@ -29,52 +29,60 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
   return employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
 
-/* Solução tirada do Discourse para converter array de objetos em um objeto
-https://forum.betrybe.com/t/reduce-converter-array-de-objetos-em-objeto/332 */
 function animalCount(species) {
-  if (!species) { 
-    const nameAndResidents = animals.map((specie) => ({ [specie.name]: specie.residents.length }));
-    return nameAndResidents.reduce((previousValue, currentValue) => Object.assign(previousValue, currentValue), {});
+  if (!species) {
+    const nameAndResidents = animals.map(specie => ({ [specie.name]: specie.residents.length }));
+    return nameAndResidents
+    .reduce((previousValue, currentValue) => Object.assign(previousValue, currentValue), {});
   }
   return animals.find(specie => specie.name === species).residents.length;
+  /* Solução tirada do Discourse para converter array de objetos em um objeto
+  https://forum.betrybe.com/t/reduce-converter-array-de-objetos-em-objeto/332 */
 }
-
+/*
+Retorna 0 se nenhum argumento for passado
+Retorna 0 se um objeto vazio for passado
+Retorna o preço total a ser cobrado dado o número de adultos, crianças e idosos*/
 function entryCalculator(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) return 0;
+  return Object.keys(entrants).reduce((previousValue, currentValue) =>
+  previousValue + (entrants[currentValue] * prices[currentValue]), 0);
+  /* Solução para verificar se um objeto está vazio tirado de:
+  https://pt.stackoverflow.com/questions/83588/em-javascript-como-verificar-que-um-objeto-est%C3%A1-vazio-sem-jquery*/
 }
 
-function animalMap(options) {
-  // seu código aqui
-}
+// function animalMap(options) {
+//   // seu código aqui
+// }
 
-function schedule(dayName) {
-  // seu código aqui
-}
+// function schedule(dayName) {
+//   // seu código aqui
+// }
 
-function oldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+// function oldestFromFirstSpecies(id) {
+//   // seu código aqui
+// }
 
-function increasePrices(percentage) {
-  // seu código aqui
-}
+// function increasePrices(percentage) {
+//   // seu código aqui
+// }
 
-function employeeCoverage(idOrName) {
-  // seu código aqui
-}
+// function employeeCoverage(idOrName) {
+//   // seu código aqui
+// }
 
 module.exports = {
   entryCalculator,
-  schedule,
+  // schedule,
   animalCount,
-  animalMap,
+  // animalMap,
   animalsByIds,
   employeeByName,
-  employeeCoverage,
+  // employeeCoverage,
   addEmployee,
   isManager,
   animalsOlderThan,
-  oldestFromFirstSpecies,
-  increasePrices,
+  // oldestFromFirstSpecies,
+  // increasePrices,
   createEmployee,
 };
