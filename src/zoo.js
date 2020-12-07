151369270e66e7e0f6dd6c8d3fed19d6f353f848
Thiago Ferreira <thiagocristhian@gmail.com>
2020-12-07 17:14:51 -0300

    Requisito 12

--- src/zoo.js
+++ src/zoo.js
@@ -85,7 +85,11 @@ function oldestFromFirstSpecies(id) {
 }
 
 function increasePrices(percentage) {
-  // seu código aqui
+  const increase = 1 + (percentage / 100);
+  Object.keys(prices).forEach((key) => {
+    prices[key] = Math.round(prices[key] * increase * 100) / 100;
+  });
+  return prices;
 }
 
 function employeeCoverage(idOrName) {
