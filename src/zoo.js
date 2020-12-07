64bec4b14ebebdb6e50a0227c18202cdf0107811
Gustavo Bregalda <gus.bregalda@gmail.com>
2020-12-07 16:02:48 -0300

    Inicio do projeto

--- src/zoo.js
+++ src/zoo.js
@@ -9,19 +9,33 @@ eslint no-unused-vars: [
 ]
 */
 
+const { animals/* , employees */ } = require('./data');
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
+function animalsByIds(...ids) {
+  const list = animals.filter(animal => ids.includes(animal.id));
+
+  return list;
 }
 
 function animalsOlderThan(animal, age) {
-  // seu código aqui
+  const animalsList = animals
+    .find(type => type.name === animal).residents
+    .every(idad => idad.age >= age);
+
+  return animalsList;
 }
 
 function employeeByName(employeeName) {
   // seu código aqui
+  // if (!employeeName) return {};
+
+  // const fullName = employees.filter(employees => employees.firstName === employeeName);
+
+  // return(fullName);
 }
+
+// console.log(employeeByName('Emery'));
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
@@ -43,8 +57,61 @@ function entryCalculator(entrants) {
   // seu código aqui
 }
 
-function animalMap(options) {
-  // seu código aqui
+function retrieveAvailableLocations() {
+  return ['N', 'E', 'S', 'W', 'NE', 'NW', 'SE', 'SW'];
+}
+
+function retrieveFilteredAnimalsByLocation(location) {
+  return animals.filter(animal => animal.location === location);
+}
+
+function retrieveAnimalsPerLocation(locations) {
+  const animalsPerLocation = {};
+
+  locations.forEach((location) => {
+    const filteredAnimals = retrieveFilteredAnimalsByLocation(location)
+      .map(animal => animal.name);
+
+    if (filteredAnimals.length !== 0) animalsPerLocation[location] = filteredAnimals;
+  });
+
+  return animalsPerLocation;
+}
+
+function retrieveAnimalsPerLocationWithNames(locations, sorted, sex) {
+  const animalsPerLocation = {};
+
+  locations.forEach((location) => {
+    const filteredAnimals = retrieveFilteredAnimalsByLocation(location).map((animal) => {
+      const animalName = animal.name;
+      const residents = animal.residents
+        .filter((resident) => {
+          const needFiltering = sex !== undefined;
+          // return needFiltering ? resident.sex === sex : true;
+          if (needFiltering) return resident.sex === sex;
+          return true;
+        })
+        .map(resident => resident.name);
+
+      if (sorted) residents.sort();
+
+      return { [animalName]: residents };
+    });
+
+    if (filteredAnimals.length !== 0) animalsPerLocation[location] = filteredAnimals;
+  });
+  return animalsPerLocation;
+}
+
+function animalMap(options = {}) {
+  const locations = retrieveAvailableLocations();
+
+  // if (!options) return retrieveAnimalsPerLocation(locations);
+
+  const { includeNames = false, sorted = false, sex } = options;
+
+  if (includeNames) return retrieveAnimalsPerLocationWithNames(locations, sorted, sex);
+  return retrieveAnimalsPerLocation(locations);
 }
 
 function schedule(dayName) {
