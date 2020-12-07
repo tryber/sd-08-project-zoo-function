dfe09fa28c5a061a21f6f7c9dc72caed777539ba
Daniel Leon <daniel.radialista@gmail.com>
2020-12-07 13:27:43 -0300

    requisito 12, ok

--- src/zoo.js
+++ src/zoo.js
@@ -79,11 +79,13 @@ function oldestFromFirstSpecies(id) {
 }
 
 function increasePrices(percentage) {
-  // seu código aqui
+  const increase = 1 + (percentage / 100);
+  Object.keys(prices).forEach((key) => {
+    prices[key] = Math.round(prices[key] * increase * 100) / 100;
+  });
 }
 
 function employeeCoverage(idOrName) {
-  // seu código aqui
 }
 
 module.exports = {
