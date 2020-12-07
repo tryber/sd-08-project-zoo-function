761713c213eb4db12168ec83842acfda4edec397
fabio-infotech <fabio35547@gmail.com>
2020-12-07 15:44:50 -0300

    requisito 9

--- src/zoo.js
+++ src/zoo.js
@@ -11,7 +11,7 @@ eslint no-unused-vars: [
 
 const data = require('../src/data');
 
-const { animals, employees, prices } = data;
+const { animals, employees, hours, prices } = data;
 
 function animalsByIds(...ids) {
   return ids.map(idFind => animals.find(animal => animal.id === idFind));
@@ -68,8 +68,53 @@ function entryCalculator(entrants = 0) {
   return total;
 }
 
+function namesBySpecieByLocation(speciesByLocation, sorted, sex) {
+  const namesBySpecie = {};
+  Object.keys(speciesByLocation).forEach((region) => {
+    const animalsByLocation = animals
+      .filter(specie => specie.location === region);
+    const arrayNamesBySpecie = animalsByLocation.map((specie) => {
+      const nameOfSpecie = specie.name;
+      // 'Com a opção `sex: female/male` especificada, retorna nomes de animais conforme o sexo'
+      const residentsBySpecie = specie.residents  
+        .filter((animal) => {
+          const sexIsDefined = sex !== undefined;
+          return (sexIsDefined ? animal.sex === sex : true);
+        })
+        .map(animal => animal.name);
+      // 'Com a opção `sorted: true`, retorna nomes de animais ordenados'
+      /* 'Com a opção `sex: \'female\'ou \'male\'` especificada e a opção `sort: true`, 
+      retorna os nomes dos animais ordenados conforme o sexo macho/fêmea' */
+      if (sorted) residentsBySpecie.sort();
+      return { [nameOfSpecie]: residentsBySpecie };
+    });
+    namesBySpecie[region] = arrayNamesBySpecie;
+  });
+  return namesBySpecie;
+}
+
 function animalMap(options) {
-  // seu código aqui
+  const speciesByLocation = {};
+  // Inicializa o objeto com as localizações e arrays vazios: { NE: [], NW: [], SE: [], SW: [] }
+  animals.forEach(({ location }) => {
+    speciesByLocation[location] = [];
+    return speciesByLocation[location];
+  });  
+  // 'Sem parâmetros, retorna animais categorizados por localização'  
+  if (!options) {
+    // Preenche cada array de speciesByLocation com os nomes dos animais
+    animals.forEach(({ name, location }) => speciesByLocation[location].push(name));    
+    return speciesByLocation;
+  }
+  // Inicializa as propriedades do parâmetro "option" com valores padrões
+  const { includeNames = false, sorted = false, sex } = options;
+  // 'Com a opção `includeNames: true`, retorna nomes de animais'
+  if (includeNames) {
+    return namesBySpecieByLocation(speciesByLocation, sorted, sex);
+  }
+  // 'Só retorna informações ordenadas e com sexo se a opção `includeNames: true` for especificada'
+  animals.forEach(({ name, location }) => speciesByLocation[location].push(name));
+  return speciesByLocation;
 }
 
 function schedule(dayName) {
