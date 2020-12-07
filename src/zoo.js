f8b6f32f9ae4e702de88905393e86ccac8aa3030
victor Farias <victorfarias.tv@gmail.com>
2020-12-07 08:04:51 -0300

    Adicionado decimo segundo requisito

--- src/zoo.js
+++ src/zoo.js
@@ -99,6 +99,11 @@ function oldestFromFirstSpecies(id) {
 
 function increasePrices(percentage) {
   // seu código aqui
+  Object.keys(prices).forEach((element) => {
+    prices[element] = Math.ceil(prices[element] *
+      (100 + percentage)) / 100;
+  });
+  return prices;
 }
 
 function employeeCoverage(idOrName) {
