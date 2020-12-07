6da1b0ccb1059f18aa3fd8b1df810a0142ff3e29
Jorge William Cardoso Furtado <jorge_william_cardoso@yahoo.com.br>
2020-12-07 16:31:09 -0300

    Envio das primeiras tarefas - 01 à 07

--- src/zoo.js
+++ src/zoo.js
@@ -9,36 +9,66 @@ eslint no-unused-vars: [
 ]
 */
 
+const { animals, employees } = require("./data");
 const data = require("./data");
 
-function animalsByIds(ids) {
+function animalsByIds(...ids) {
   // seu código aqui
+  if (!ids) return [];
+  return animals.filter((animal) => ids.find((id) => animal.id === id));
 }
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
+  return animals
+    .find((a) => (a.name = animal))
+    .residents.every((resident) => resident.age >= age);
 }
 
 function employeeByName(employeeName) {
   // seu código aqui
+  if (!employeeName) return {};
+  return employees.find(
+    (name) => name.firstName === employeeName || name.lastName === employeeName
+  );
 }
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
+  return { ...personalInfo, ...associatedWith };
 }
 
 function isManager(id) {
   // seu código aqui
+  return employees.some((someId) => someId.managers.includes(id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+function addEmployee(
+  id,
+  firstName,
+  lastName,
+  managers = [],
+  responsibleFor = []
+) {
   // seu código aqui
+  employees.push({
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  });
 }
 
 function animalCount(species) {
   // seu código aqui
+  if (!species)
+    return animals.reduce((acc, animal) => {
+      acc[animal.name] = animal.residents.length;
+      return acc;
+    }, {});
+  return animals.find((animal) => animal.name === species).residents.length;
 }
-
 function entryCalculator(entrants) {
   // seu código aqui
 }
