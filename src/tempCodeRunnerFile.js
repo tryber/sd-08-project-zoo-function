67125fe7c5c875ae1b28b1818821feb801d87156
Mateus Coury <mateuscoury@gmail.com>
2020-12-07 17:32:04 -0300

    entrycalc

---
+++ src/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+const entrantss = { 'Adult': 2, 'Child': 3, 'Senior': 1 };
+  const array1 = Object.entries(entrantss)
+  console.log(array1)
--- src/zoo.js
+++ src/zoo.js
@@ -57,47 +57,78 @@ function animalCount(species) {
 }
 
 function entryCalculator(entrants) {
+  return entrants
+    ? Object.keys(entrants).reduce(
+      (acc, current) =>
+        acc + (data.prices[current] * entrants[current]),
+      0,
+    )
+    : 0;
 }
 
 function animalMap(options) {
+  /* 
+    const animalsporregiao = data.animals.filter(element => element.location === element.name)
+    console.log(animalsporregiao)
+  
+   */
+
+  /* const residenname = data.animals.residents.reduce((acc, current) => [acc.name + current.name])
+  console.log(residenname)
+  const animals = data.animals.map(element => `{${animals.name}:${residenname}}`)
+ */
+
 }
 
 function schedule(dayName) {
   // seu código aqui
-}
+  const { hours } = data
+  const crono = Object.entries(hours)
+  const dayMsg = (dayOfWeek) => {
+    if (dayOfWeek === 'Monday') return 'CLOSED';
+    return `Open from ${hours[dayOfWeek].open}am until ${hours[dayOfWeek].close - 12}pm`;
+  };
+  const cronograma = () => {
+    const mensagem = crono.reduce((acc, [keyDay, value]) => {
+      acc[keyDay] = dayMsg(keyDay);
+      return acc;
+    }, {});
 
-function oldestFromFirstSpecies(id) {
-  // seu código aqui
-  const func1 = data.employees.find(func => func.id === id);
-  const primeiradalista = func1.responsibleFor[0];
-  const allFromFirst = animals.find(animal =>
-    primeiradalista.includes(animal.id)).residents;
-  const oldestAnimalObj = allFromFirst.sort((a, b) =>
-    b.age - a.age)[0];
-  const { name, sex, age } = oldestAnimalObj;
-  return [name, sex, age];
-}
 
-function increasePrices(percentage) {
-  // seu código aqui
-}
+  }
 
-function employeeCoverage(idOrName) {
-  // seu código aqui
-}
+  function oldestFromFirstSpecies(id) {
+    // seu código aqui
+    const func1 = data.employees.find(func => func.id === id);
+    const primeiradalista = func1.responsibleFor[0];
+    const allFromFirst = animals.find(animal =>
+      primeiradalista.includes(animal.id)).residents;
+    const oldestAnimalObj = allFromFirst.sort((a, b) =>
+      b.age - a.age)[0];
+    const { name, sex, age } = oldestAnimalObj;
+    return [name, sex, age];
+  }
 
-module.exports = {
-  entryCalculator,
-  schedule,
-  animalCount,
-  animalMap,
-  animalsByIds,
-  employeeByName,
-  employeeCoverage,
-  addEmployee,
-  isManager,
-  animalsOlderThan,
-  oldestFromFirstSpecies,
-  increasePrices,
-  createEmployee,
-};
+  function increasePrices(percentage) {
+    // seu código aqui
+  }
+
+  function employeeCoverage(idOrName) {
+    // seu código aqui
+  }
+
+  module.exports = {
+    entryCalculator,
+    schedule,
+    animalCount,
+    animalMap,
+    animalsByIds,
+    employeeByName,
+    employeeCoverage,
+    addEmployee,
+    isManager,
+    animalsOlderThan,
+    oldestFromFirstSpecies,
+    increasePrices,
+    createEmployee,
+  };
