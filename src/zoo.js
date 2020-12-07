9725c877192276fcb577335bc7f4a6e13bd02493
Mateus Coury <mateuscoury@gmail.com>
2020-12-07 14:03:12 -0300

    arquivos novos

--- src/zoo.js
+++ src/zoo.js
@@ -38,22 +38,41 @@ function createEmployee(personalInfo, associatedWith) {
 
 function isManager(id) {
   // seu código aqui
+  return data.employees.filter(employee =>
+    employee.managers.includes(id)).length > 0;
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
+
 }
 
 function animalCount(species) {
   // seu código aqui
+  if (species === undefined) {
+    return data.animals.filter(animal1 => `${animal1.name} : ${animal1.residents.length}`)
+  }
+  const filtroanimal = data.animals.find(animal => animal.name === species)
+  return filtroanimal.residents.length
 }
 
 function entryCalculator(entrants) {
-  // seu código aqui
+  if (entrants === undefined) {
+    return 0
+  }
+
+
+
 }
 
 function animalMap(options) {
   // seu código aqui
+  if (options === undefined) {
+    const filtro = data.animals.filter(animal1 => animal1.location === animal1.name)
+    console.log(filtro)
+    return filtro.map(animal => `${animal.location}:${filtro.name}`)
+  }
+
 }
 
 function schedule(dayName) {
@@ -62,6 +81,17 @@ function schedule(dayName) {
 
 function oldestFromFirstSpecies(id) {
   // seu código aqui
+  const func = data.employees.find(func => func.id === id) //achafuncionario
+  const primeiradalista = func.responsibleFor[0];
+  const allFromFirst = animals.find(animal =>
+    primeiradalista.includes(animal.id)).residents;
+  const oldestAnimalObj = allFromFirst.sort((a, b) =>
+    b.age - a.age)[0];
+  const { name, sex, age } = oldestAnimalObj;
+
+  return [name, sex, age];
+
+
 }
 
 function increasePrices(percentage) {
