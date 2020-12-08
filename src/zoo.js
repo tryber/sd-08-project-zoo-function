c474adedf5565b9ea9d262f3c9fd83bc03e99105
Joao <joaocarlitoneto@gmail.com>
2020-12-07 22:30:27 -0300

    Send Exercise 10/11/12  without issues

--- src/zoo.js
+++ src/zoo.js
@@ -1,5 +1,5 @@
 // const data = require('./data');
-const { animals, employees, prices } = require('./data');
+const { animals, employees, prices, hours } = require('./data');
 
 function animalsByIds(...ids) {
   if (!ids) return [];
@@ -39,10 +39,7 @@ function animalCount(species) {
   /* Solução tirada do Discourse para converter array de objetos em um objeto
   https://forum.betrybe.com/t/reduce-converter-array-de-objetos-em-objeto/332 */
 }
-/*
-Retorna 0 se nenhum argumento for passado
-Retorna 0 se um objeto vazio for passado
-Retorna o preço total a ser cobrado dado o número de adultos, crianças e idosos*/
+
 function entryCalculator(entrants) {
   if (!entrants || Object.keys(entrants).length === 0) return 0;
   return Object.keys(entrants).reduce((previousValue, currentValue) =>
@@ -55,17 +52,27 @@ function entryCalculator(entrants) {
 //   // seu código aqui
 // }
 
-// function schedule(dayName) {
-//   // seu código aqui
-// }
+function schedule(dayName) {
+  const operatingDays = Object.entries(hours).reduce((previousValue, [key, value]) => {
+    previousValue[key] = (value.close - value.open > 0) ? `Open from ${value.open}am until ${value.close - 12}pm` : 'CLOSED';
+    return previousValue;
+  }, {});
+  if (operatingDays[dayName]) return { [dayName]: operatingDays[dayName] };
+  return operatingDays;
+}
 
-// function oldestFromFirstSpecies(id) {
-//   // seu código aqui
-// }
+function oldestFromFirstSpecies(id) {
+  const animalId = employees.find(employee => employee.id === id).responsibleFor[0];
+  const { name, sex, age } = animals.find(animal =>
+    animal.id === animalId).residents.sort((a, b) => b.age - a.age)[0];
+  return [name, sex, age];
+}
 
-// function increasePrices(percentage) {
-//   // seu código aqui
-// }
+function increasePrices(percentage) {
+  return Object.keys(prices).forEach((element) => {
+    prices[element] = Math.ceil(prices[element] * (100 + percentage)) / 100;
+  });
+}
 
 // function employeeCoverage(idOrName) {
 //   // seu código aqui
@@ -73,7 +80,7 @@ function entryCalculator(entrants) {
 
 module.exports = {
   entryCalculator,
-  // schedule,
+  schedule,
   animalCount,
   // animalMap,
   animalsByIds,
@@ -82,7 +89,7 @@ module.exports = {
   addEmployee,
   isManager,
   animalsOlderThan,
-  // oldestFromFirstSpecies,
-  // increasePrices,
+  oldestFromFirstSpecies,
+  increasePrices,
   createEmployee,
 };
