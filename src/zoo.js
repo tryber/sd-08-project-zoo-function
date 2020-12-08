6756c786771d374f3ac2db67de2dfb6a53615dfa
Felip França <felipefrancafj@gmail.com>
2020-12-08 20:33:51 -0300

    Requisito 9

--- src/zoo.js
+++ src/zoo.js
@@ -72,7 +72,10 @@ function entryCalculator(entrants) {
 }
 
 function animalMap(options) {
-  // seu código aqui
+  if (!options) return listNameAnimals(locations());
+  const { includeNames } = options;
+  if (includeNames) return getNameAnimals(options);
+  return listNameAnimals(locations());
 }
 
 function schedule(dayName) {
@@ -90,6 +93,38 @@ function increasePrices(percentage) {
 function employeeCoverage(idOrName) {
   // seu código aqui
 }
+
+const locations = () => data.animals.map(el => el.location)
+  .filter((el, index, arr) => arr.indexOf(el) === index);
+
+const isLocalName = locals => data.animals.filter(el => el.location === locals)
+  .map(el => el.name);
+
+const listNameAnimals = allLocals => allLocals.reduce((acumulator, value) => {
+  acumulator[value] = isLocalName(value);
+  return acumulator;
+}, {});
+
+const getNamesAnimalarr = classAnimal => classAnimal.map(animal => animal.name);
+const getNamesAnimalsex = (classAnimal, sex) => classAnimal.filter(animal => animal.sex === sex)
+  .map(animal => animal.name);
+
+const isSex = (residents, sex) => (
+  sex !== undefined ? getNamesAnimalsex(residents, sex) : getNamesAnimalarr(residents));
+
+const getNameAnimals = (args) => {
+  const { sex, sorted } = args;
+  const result = data.animals.reduce((acumulador, animal) => {
+    if (!acumulador[animal.location]) acumulador[animal.location] = [];
+    const { name: key, residents } = animal;
+    const objNames = {};
+    const arr = isSex(residents, sex);
+    objNames[key] = sorted ? arr.sort() : arr;
+    acumulador[animal.location].push(objNames);
+    return acumulador;
+  }, {});
+  return result;
+};
 
 module.exports = {
   entryCalculator,
