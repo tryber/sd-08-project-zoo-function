75e2bc1eb2416a8d10772853642361d97fe63b4b
Victor Moraes <moraesvictorgk@gmail.com>
2020-12-08 18:38:21 -0300

    increase princes tentativa

--- src/zoo.js
+++ src/zoo.js
@@ -150,11 +150,11 @@ console.log(oldestFromFirstSpecies('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 function increasePrices(percentage) {
   const chaves = Object.keys(prices);
   const porcentagem = 1 + (percentage / 100);
-  chaves.forEach(element => {
+  chaves.forEach((element) => {
     prices[element] = Math.round(prices[element] * (porcentagem) * 100) / 100;
-    
   });
   return prices;
+
   // seu código aqui
 }
 console.log(increasePrices(50));
