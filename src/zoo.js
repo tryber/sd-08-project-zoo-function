9f3705b15ded0423dac73f6b2f1bd14691ffc19e
wagnerberna <cabelo29007@gmail.com>
2020-12-08 05:26:43 -0300

    refatorado pós plantão

--- src/zoo.js
+++ src/zoo.js
@@ -13,33 +13,35 @@ const { animals } = require('./data');
 const data = require('./data');
 
 function animalsByIds(...ids) {
-  let retorno = [];
-//  console.log(ids);
+  //let retorno = [];
+  // console.log(ids);
   if (ids.length === 0) {
     return [];
   } else {
-    ids.forEach((element, index) => {
-      const filterAnimals = animals.filter(({id}) => (element === id));
-      retorno[index] = [...filterAnimals];
-      });
-    console.log(Object.values(retorno[1]));
+    // ids.forEach((element, index) => {
+    //   const filterAnimals = animals.filter(({id}) => (element === id));
+    //   retorno[index] = {...filterAnimals};
+    //   });
+    //retorno = animals.filter(actua => actua.id === ids[0] || actua.id === ids[1]);
+    retorno = animals.filter(({id}) => ids[0] === id || ids[1] === id);
+    console.log(Object.values(retorno));
     return retorno;
   }
 }
-//animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46');
+animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46');
 
 function animalsOlderThan(animal, age) {
   let retorno;
   let filterName = animals.filter(({name}) => (name === animal));
   console.log(filterName);
-     let teste = animals.every(({residents}) => (residents.age >= age));
-  console.log(teste);
+  //    let teste = animals.every(({residents}) => (residents.age >= age));
+  // console.log(teste);
 
   retorno = filterName.every(({residents}) => residents.age >= age);
   console.log(retorno);
   return retorno;
 }
-animalsOlderThan('otters', 7);
+//animalsOlderThan('otters', 7);
 
 function employeeByName(employeeName) {
   // seu código aqui
