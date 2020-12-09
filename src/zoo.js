d436f6a56821fc88f14ad0df1468c13e7a385a37
wesleypribeiro <wesley_paulo00@hotmail.com>
2020-12-08 22:51:29 -0300

    10 - Concluido

--- src/zoo.js
+++ src/zoo.js
@@ -81,12 +81,87 @@ function entryCalculator(entrants) {
 }
 
 function animalMap(options) {
-  // seu código aqui
+  /*  let teste;
+  if (!options) {
+    teste = data.animals.reduce((animalRegion, animal) => {
+      if (!animalRegion[animal.location]) { animalRegion[animal.location] = []; }
+      animalRegion[animal.location].push(animal.name);
+      return animalRegion;
+    }, {});
+  }
+
+  else if (options.includeNames) {
+    teste = data.animals.reduce((animalRegion, animal) => {
+      if (!animalRegion[animal.location]) { animalRegion[animal.location] = []; }
+      let animalName = {};
+
+      if (!animalName[animal.name]) { animalName[animal.name] = []; }
+
+      animal.residents.forEach((teste) => animalName[animal.name].push(teste.name));
+      animalRegion[animal.location].push(animalName);
+      return animalRegion;
+    }, {});
+  }
+
+  else if (options.sorted) {
+    teste = data.animals.reduce((animalRegion, animal) => {
+      if (!animalRegion[animal.location]) { animalRegion[animal.location] = []; }
+      let animalName = {};
+
+      if (!animalName[animal.name]) { animalName[animal.name] = []; }
+
+
+      //let nemsei = animalName[animal.name].sort((a, b) => a > b);
+      animal.residents.forEach((teste) => animalName[animal.name].push(teste.name));
+      animalName[animal.name] = animalName[animal.name].sort();
+      animalRegion[animal.location].push(animalName);
+      return animalRegion;
+    }, {});
+
+    console.log(Object.keys(teste));
+  }
+
+  return teste;*/
+}
+
+function timeConvert(time) {
+  time = time.toString().match(/^([01]\d|2[0-3])/) || [time];
+
+  if (time.length > 1) {
+    time = time.slice(1);
+    time[0] = +time[0] % 12 || 12;
+  }
+  return time.join('');
+}
+
+function getDay(day, openHour, closeHour) {
+  if (day === 'Monday') {
+    return 'CLOSED';
+  }
+  return `Open from ${openHour}am until ${timeConvert(closeHour)}pm`;
 }
 
 function schedule(dayName) {
-  // seu código aqui
+  const { hours } = data;
+  let days;
+  if (!dayName) {
+    days = Object.entries(hours).reduce((arr, current) => {
+      arr[current[0]] = getDay(current[0], current[1].open, current[1].close);
+      return arr;
+    }, {});
+  } else {
+    days = Object.entries(hours).reduce((arr, current) => {
+      if (current[0] === dayName) {
+        arr[current[0]] = getDay(current[0], current[1].open, current[1].close);
+      }
+      return arr;
+    }, {});
+  }
+
+  return days;
 }
+
+console.log(schedule());
 
 function oldestFromFirstSpecies(id) {
   // seu código aqui
