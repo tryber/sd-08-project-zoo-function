6c9fa0ee1d10074c26d8145b069e643572bc506e
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2020-12-09 20:18:21 -0300

    Animals Map

--- src/zoo.js
+++ src/zoo.js
@@ -11,41 +11,172 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
+function animalsByIds(...ids) {
+  return ids.map(id => data.animals.find(animal => animal.id === id));
 }
 
 function animalsOlderThan(animal, age) {
-  // seu código aqui
+  return data.animals.filter(animalObj => animalObj.name === animal)
+    .every(animal => animal.residents.every((resident => resident.age > age)));
 }
 
 function employeeByName(employeeName) {
-  // seu código aqui
+  return data.employees.find(employee => employee.firstName === employeeName ||
+    employee.lastName === employeeName) || {};
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  const { id, firstName, lastName } = personalInfo;
+  const { managers, responsibleFor } = associatedWith;
+  return ({ id, firstName, lastName, managers, responsibleFor });
 }
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.reduce((acc, val) => acc.concat(val.managers), [])
+    .some(val => val === id);
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  data.employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
 
 function animalCount(species) {
-  // seu código aqui
+  if (typeof species === 'undefined') return data.animals.reduce((acc, val) => {
+    acc[val.name] = val.residents.length;
+    return acc;
+  }, {});
+  return data.animals.find(specie => specie.name == species).residents.length;
 }
 
 function entryCalculator(entrants) {
-  // seu código aqui
+  if (typeof entrants === 'object')
+    return Object.keys(entrants).reduce((acc, val) => acc + entrants[val] * data.prices[val], 0);
+  else return 0;
 }
 
-function animalMap(options) {
-  // seu código aqui
+/*function animalMap(options = {}) {
+  const {includeNames, sorted, sex} = options;
+  return data.animals.reduce((locations, animal) => (locations.includes(animal.location))
+    ? locations : [...locations, animal.location], [])
+      .reduce((finalObject, location) => {
+        finalObject[location] = data.animals.filter(animal => animal.location === location)
+          .reduce((finalArray, anima) => [...finalArray, ((includeNames) ?
+            ({[anima.name]: ((st, sx, rs, rsn, rsx, stf, mpf, ftf) =>
+              ((st) ? ((sx) ? (rs[ftf](rsx)[mpf](rsn)[stf]()) :
+                (rs[mpf](rsn)[stf]())) : (sx) ? (rs[ftf](rsx)[mpf](rsn))
+                  : (rs[mpf](rsn))))
+                    (sorted, sex, anima.residents, resident => resident.name,
+                      resident => resident.sex === sex, 'sort', 'map', 'filter')}) :
+                anima.name)], []);
+        return finalObject;
+      }, {});
+}*/
+
+Object.prototype.deepMerge = function(data) {
+  if (Array.isArray(this)) {
+    if (Array.isArray(data)) {
+      data.forEach(element => {
+        if (!this.includes(element)) this.push(element);
+      })
+    } else {
+      Object.assign(this, data);
+    }
+  } else {
+    Object.entries(data).forEach(([key, value]) => {
+      if (typeof this[key] == 'object') {
+        this[key] = this[key].deepMerge(value);
+      } else {
+        this[key] = value;
+      }
+    });
+  }
+  return this;
 }
+
+Object.prototype.transformIf = function(condition, lambda) {
+  if (condition) {
+    return lambda(this);
+  }
+  return this;
+}
+
+function animalMap(opts) {
+  if (typeof opts === 'undefined') opts = {};
+  const {includeNames, sex, sorted} = opts;
+  return data.animals.reduce((acc, animal) => {
+    return acc.deepMerge({
+      [animal.location] : [
+      animal
+      .transformIf(includeNames,
+        animal =>
+        ({[animal.name]:
+          animal.residents
+          .transformIf(sex, residents => residents.filter(resident => resident.sex === sex))
+          .transformIf(true, residents => residents.map(resident => resident.name))
+          .transformIf(sorted, residents => residents.sort())}))
+      .transformIf(!includeNames, animal => animal.name)
+      ]});
+  }, {});
+}
+
+/*Object.prototype.transformIf = function (condition, [successFn, successArgs], [failFn, failArgs]) {
+  return (condition) ? this[successFn](successArgs) : this[failFn](failArgs);
+}*/
+
+/*
+function animalMap(options = {}) {
+  const { includeNames, sex, sorted } = options;
+  return data.animals.reduce((acc, animal) =>
+    deepMerge(acc,
+      ({
+        [animal.location]: [
+        animal.transformIf(includeNames,
+          ['justDoIt', animal => ({
+            [animal.name]:
+            animal.residents
+              .transformIf(sex,
+                ['filter', resident => resident.sex == sex],
+                ['doNothing', undefined])
+                  .map(resident => resident.name)
+                  .transformIf(sorted,
+                    ['sort', undefined],
+                    ['doNothing', undefined])
+          })],
+          ['justDoIt', animal => animal.name]) ]
+      }))
+    , {});
+}*/
+
+/*
+function animalMap(opt) {
+  const options = {
+    includeNames: false,
+    sorted: false,
+    sex: '',
+  };
+  Object.assign(options, opt);
+  return data.animals
+    .reduce(
+      (locations, animal) => (locations.includes(animal.location) ? locations : [...locations, animal.location]),
+      [],
+    )
+    .reduce((finalObject, location) => {
+      finalObject[location] = data.animals
+        .filter(animal => animal.location === location)
+        .reduce(
+          (finalArray, animal) => [
+            ...finalArray,
+            options.includeNames
+              ? {
+                [animal.name]: animal.residents.map(resident => resident.name),
+              }
+              : animal.name,
+          ],
+          [],
+        );
+      return finalObject;
+    }, {});
+}*/
 
 function schedule(dayName) {
   // seu código aqui
@@ -78,4 +209,3 @@ module.exports = {
   increasePrices,
   createEmployee,
 };
-
