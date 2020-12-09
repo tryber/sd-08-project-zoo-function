99d30c2172cbf8af7db97f53a6b4c85eebaf492c
Mateus Coury <mateuscoury@gmail.com>
2020-12-09 19:39:09 -0300

    cc employee

--- src/tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-function employeeCoverage(idOrName) {
-  const func = data.employees.find(func => func.id === idOrName).responsibleFor
-  return func
-
-}
-console.log(employeeCoverage('4b40a139-d4dc-4f09-822d-ec25e819a5ad'))
