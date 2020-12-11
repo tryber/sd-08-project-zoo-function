4cde5a72136bb66dadb996bd97feaf78d2a90b2c
Thiago da Silva Gomes <thiagosilvagomes@gmail.com>
2020-12-10 21:41:04 -0300

    Requisito 2(2)

--- src/zoo.js
+++ src/zoo.js
@@ -22,10 +22,9 @@ function animalsByIds(...ids) {
   return animById;
 }
 
-function animalsOlderThan(animal, age) {  
-  let test = data.animals.filter(elem => elem.name == animal)
+function animalsOlderThan(animal, age) {
+  return data.animals.filter(elem => elem.name === animal)
   .every((elem1, index) => elem1.residents[index].age > age);
-  return (test);
 }
 
 function employeeByName(employeeName) {
