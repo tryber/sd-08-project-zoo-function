11295a5f22e10a14a0fbea175685a65e3f36746a
Nícolas Tozato <ntozato9@gmail.com>
2020-12-10 21:10:27 -0300

    oldestFromFirstSpecies, animalCount, increasePrices

--- src/zoo.js
+++ src/zoo.js
@@ -23,8 +23,8 @@ function animalsByIds(...ids) {
 function animalsOlderThan(animal, age) {
   // seu código aqui
   return animals
-    .find(elem => elem.name === animal)
-    .residents.every(elem => elem.age >= age);
+    .find((elem) => elem.name === animal)
+    .residents.every((elem) => elem.age >= age);
 }
 
 function employeeByName(employeeName) {
@@ -32,7 +32,9 @@ function employeeByName(employeeName) {
   if (!employeeName) {
     return {};
   }
-  return employees.find(elem => elem.firstName === employeeName || elem.lastName === employeeName);
+  return employees.find(
+    (elem) => elem.firstName === employeeName || elem.lastName === employeeName
+  );
 }
 
 function createEmployee(personalInfo, associatedWith) {
@@ -54,22 +56,46 @@ function isManager(id) {
   return employees.some((elem, index) => elem.managers[index] === id);
 }
 
-function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+function addEmployee(
+  id,
+  firstName,
+  lastName,
+  managers = [],
+  responsibleFor = []
+) {
   // seu código aqui
-  return employees.push({ id, firstName, lastName, managers, responsibleFor });
+  const newObj = {
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  };
+  employees.push(newObj);
 }
 
 function animalCount(species) {
   // seu código aqui
+  const newObj = {};
+  if (!species) {
+    animals.forEach(elem => {
+      const obj = {
+        [elem.name]: elem.residents.length,
+      };
+      Object.assign(newObj, obj);
+    });
+    return newObj;
+  }
+  return animals.find(elem => elem.name === species).residents.length;
 }
 
 function entryCalculator(entrants) {
   // seu código aqui
-  if (!entrants || entrants === {}) {
+  if (!entrants) {
     return 0;
   }
   const { Adult = 0, Child = 0, Senior = 0 } = entrants;
-  const sum = (prices.Adult * Adult) + (prices.Child * Child) + (prices.Senior * Senior);
+  const sum = (Adult * prices.Adult) + (Child * prices.Child) + (Senior * prices.Senior);
   return sum;
 }
 
@@ -83,10 +109,27 @@ function schedule(dayName) {
 
 function oldestFromFirstSpecies(id) {
   // seu código aqui
+  const animalId = employees.find(employee => employee.id === id).responsibleFor[0];
+  const animal = animals.find(specie => specie.id === animalId);
+  let oldestAge = 0;
+  let oldestAnimal;
+  animal.residents.forEach(elem => {
+    if (elem.age > oldestAge) {
+      oldestAge = elem.age;
+      oldestAnimal = Object.values(elem);     
+    }
+  });
+  return oldestAnimal;
 }
 
 function increasePrices(percentage) {
   // seu código aqui
+  const adult = (percentage / 100 * prices.Adult) + prices.Adult;
+  const child = (percentage / 100 * prices.Child) + prices.Child;
+  const senior = (percentage / 100 * prices.Senior) + prices.Senior;
+  prices.Adult = Math.round(adult * 100) / 100;
+  prices.Child = Math.round(child * 100) / 100;
+  prices.Senior = Math.round(senior * 100) / 100;
 }
 
 function employeeCoverage(idOrName) {
