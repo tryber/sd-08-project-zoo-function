456e098e7ffa0275cdfbc22141ebf3fd32cdbc2c
Thiago da Silva Gomes <thiagosilvagomes@gmail.com>
2020-12-10 22:36:05 -0300

    Requisito 4(1)

--- src/zoo.js
+++ src/zoo.js
@@ -34,10 +34,29 @@ function employeeByName(employeeName) {
   return data.employees.find(elem =>
   (elem.firstName === employeeName || elem.lastName === employeeName));
 }
-console.log(employeeByName('Nigel'));
+
+const personalInfo1 = {
+  id: '7ed1c9bb-8570-44f6-b718-0666b869573a',
+  firstName: 'John',
+  lastName: 'Doe',
+};
+
+const associatedWith1 = {
+  managers: [
+    'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1',
+    '9e7d4524-363c-416a-8759-8aa7e50c0992'
+  ],
+  responsibleFor: [
+    '0938aa23-f153-4937-9f88-4858b24d6bce',
+    '89be95b3-47e4-4c5b-b687-1fabf2afa274',
+    'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'
+  ]
+};
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  const { id, firstName, lastName } = personalInfo;
+  const { managers, responsibleFor } = associatedWith;
+  return { id, firstName, lastName, managers, responsibleFor };
 }
 
 function isManager(id) {
