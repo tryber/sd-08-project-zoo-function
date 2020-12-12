566558a570604226e1f05bc7ea989a88b6756a82
Thiago da Silva Gomes <thiagosilvagomes@gmail.com>
2020-12-11 23:56:18 -0300

    Requisito 7(4)

--- src/zoo.js
+++ src/zoo.js
@@ -72,8 +72,8 @@ function animalCount(species) {
 }
 
 function entryCalculator(entrants) {
-  if ( entrants==null || entrants==={} ) {
-    return 0;
+  if ( entrants == null || entrants === {} ) {
+    return (0);
   }
   const { Adult = 0, Child = 0, Senior = 0 } = entrants;
   return ((Adult * 49.99) + (Child * 20.99) + (Senior * 24.99));
