dd48bbf5d63907b52995b3cb49f844be91731eb2
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2020-12-15 14:46:13 -0300

    corrigindo requisito

--- src/zoo.js
+++ src/zoo.js
@@ -36,8 +36,9 @@ function createEmployee(personalInfo, associatedWith) {
 function isManager(id) {
   data.employees.forEach((employee) => {
     if (employee.managers.includes(id)) return true;
-    return false;
+    return;
   });
+  return false;
 }
 function addEmployee(
   id,
