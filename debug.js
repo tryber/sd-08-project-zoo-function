4e6bd574040b726b8b7cf7ced81d3a333d63203a
vivianeflowt <vivianeflowt@gmail.com>
2020-12-14 21:56:23 -0300

    ok

--- debug.js
+++ debug.js
@@ -1,13 +1,6 @@
+const { animals } = require('./src/data');
 const data = require('./src/data');
 
-function animalsOlderThan(animal, age) {
-  return data.animals.find(a => a.name === animal).residents.every(r => r.age > age);
-}
-
-const result = animalsOlderThan('otters', 7);
-// const result = animalsOlderThan('penguins', 10);
-
-// const result = data
-// const result = data.animals[3].residents.every(r => r.age >.animals[4].residents.every(r => r.age > 7); 10);
-
-console.log(result);
+// function animalsOlderThan(animal, age) {
+//   return data.animals.find(a => a.name === animal).residents.every(r => r.age > age);
+// }
