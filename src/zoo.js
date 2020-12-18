75388621e8a95fe7ec7932f8b9591e92f3dabce5
brutibbc <brutibbc@gmail.com>
2020-12-18 08:11:28 -0300

    Requisito 8

--- src/zoo.js
+++ src/zoo.js
@@ -9,7 +9,7 @@ eslint no-unused-vars: [
 ]
 */
 
-const { animals, employees } = require('./data');
+const { animals, employees, prices } = require('./data');
 const data = require('./data');
 
 
@@ -68,7 +68,8 @@ function animalCount(species) {
 }
 
 function entryCalculator(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) return 0;
+  return Object.keys(entrants).reduce((acc, current) => acc + (entrants[current] * prices[current]), 0);
 }
 
 function animalMap(options) {
