6f8a1d59d42a58affae792ea1ce554f9b00cd316
Marcela <marcela9908@gmail.com>
2020-12-18 21:46:53 -0300

    Correçao codeclimate

--- src/zoo.js
+++ src/zoo.js
@@ -48,11 +48,11 @@ function isManager(id) {
 
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
   return employees.push({
-    id: id,
-    firstName: firstName,
-    lastName: lastName,
-    managers: managers,
-    responsibleFor: responsibleFor,
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
   });
 }
 
