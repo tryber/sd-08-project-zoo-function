67616cca7c5cbd5a1345c7f6eb758ec48b0848fd
Lucas Neiva <lucassneiva.code@gmail.com>
2020-12-29 17:40:59 -0300

    add climated

--- src/zoo.js
+++ src/zoo.js
@@ -51,62 +51,51 @@ function isManager(id) {
 }
 
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
-    const personalInfo = { id, firstName, lastName };
-    const associatedWith = { managers, responsibleFor };
-    employees.push(createEmployee(personalInfo, associatedWith));
-};
-
-function animalCount(species) {
-  // if (!species) {
-  //   const result = animals.map((animals), ((index) => {
-  //     const geral = 
-  //     {animals.name: animals.name.residents.length}
-  //     return geral;
-  //   })
-  // const result = animals.reduce(name => (
-  //     species.includes(name) && species.includes('number')
-
-  //   ));
-  //   return result;
-  // }
+  const personalInfo = { id, firstName, lastName };
+  const associatedWith = { managers, responsibleFor };
+  employees.push(createEmployee(personalInfo, associatedWith));
 }
 
-  function entryCalculator(entrants) {
-    // seu código aqui
-  }
+function animalCount(species) {
 
-  function animalMap(options) {
-    // seu código aqui
-  }
+}
 
-  function schedule(dayName) {
-    // seu código aqui
-  }
+function entryCalculator(entrants) {
+  // seu código aqui
+}
 
-  function oldestFromFirstSpecies(id) {
-    // seu código aqui
-  }
+function animalMap(options) {
+  // seu código aqui
+}
 
-  function increasePrices(percentage) {
-    // seu código aqui
-  }
+function schedule(dayName) {
+  // seu código aqui
+}
 
-  function employeeCoverage(idOrName) {
-    // seu código aqui
-  }
+function oldestFromFirstSpecies(id) {
+  // seu código aqui
+}
 
-  module.exports = {
-    entryCalculator,
-    schedule,
-    animalCount,
-    animalMap,
-    animalsByIds,
-    employeeByName,
-    employeeCoverage,
-    addEmployee,
-    isManager,
-    animalsOlderThan,
-    oldestFromFirstSpecies,
-    increasePrices,
-    createEmployee,
-  };
+function increasePrices(percentage) {
+  // seu código aqui
+}
+
+function employeeCoverage(idOrName) {
+  // seu código aqui
+}
+
+module.exports = {
+  entryCalculator,
+  schedule,
+  animalCount,
+  animalMap,
+  animalsByIds,
+  employeeByName,
+  employeeCoverage,
+  addEmployee,
+  isManager,
+  animalsOlderThan,
+  oldestFromFirstSpecies,
+  increasePrices,
+  createEmployee,
+};
