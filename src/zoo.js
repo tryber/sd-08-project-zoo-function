2b940ffc22e8c49f0c302fd710a62be3e8db9373
rodolfo_oliveira <rod.vgo@gmail.com>
2020-12-30 03:19:02 -0300

    ajustes

--- src/zoo.js
+++ src/zoo.js
@@ -60,7 +60,7 @@ function animalMap(options) {
 }
 
 function schedule(dayName) {
-  const ret = Object.entries(dayName).reduce((acc, [key, value]) => {
+  const ret = Object.entries(hours).reduce((acc, [key, value]) => {
     const { open, close } = value;
     acc[key] = close - open > 0 ? `Open from ${open}pm until ${close % 12}pm` : 'CLOSED';
     return acc;
@@ -68,6 +68,8 @@ function schedule(dayName) {
   if (typeof dayName === 'string' && dayName.length !== 0) return { [dayName]: ret[dayName] };
   return ret;
 }
+
+console.log(schedule('Monday'))
 
 function oldestFromFirstSpecies(id) {
   // seu código aqui
