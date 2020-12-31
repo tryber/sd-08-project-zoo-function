c8afba4dead23afc869bbd035580518b1a7b3872
Bruno <brunocmoraes97@gmail.com>
2020-12-31 15:56:34 -0300

    Correção code climate

--- src/zoo.js
+++ src/zoo.js
@@ -11,7 +11,7 @@ eslint no-unused-vars: [
 //
 
 const data = require('./data');
-const { animals, employees, prices } = require('./data');
+const { animals, employees, prices, hours } = require('./data');
 
 function animalsByIds(...ids) {
   return animals.filter(animal => ids.find(id => animal.id === id));
@@ -59,23 +59,24 @@ function animalCount(species) {
 }
 
 function entryCalculator(entrants) {
-  if(!entrants)  return 0;
-  const { Adult = 0, Senior = 0, Child = 0} = entrants;
+  if (!entrants) return 0;
+  const { Adult = 0, Senior = 0, Child = 0 } = entrants;
   const adultValor = prices.Adult * Adult;
   const seniorValor = prices.Senior * Senior;
   const childValor = prices.Child * Child;
   return (adultValor + seniorValor + childValor).toFixed(2);
-  
 }
-console.log(entryCalculator())
+
 function animalMap(options) {
   // seu código aqui
 }
 
 function schedule(dayName) {
-  // seu código aqui
+  if (!dayName) {
+    return hours.find(days => days.includes(dayName))
+  }
 }
-
+console.log(schedule('Sunday'));
 function oldestFromFirstSpecies(id) {
   // seu código aqui
 }
