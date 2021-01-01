4dc6314348efb70f2debe800f66e917b5de6ddd6
Blucilha <blucilha.nascimento@gmail.com>
2021-01-01 09:28:19 -0300

    Tentando fazer o exercicio 5

--- src/zoo.js
+++ src/zoo.js
@@ -40,7 +40,7 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  // seu código aqui
+    return employees.some(manager => manager.managers.includes(id));
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
