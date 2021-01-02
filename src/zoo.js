0080c1f5d035a6f042e5e9cf402ad9954cf338bf
wend3ll-souza <wend3llsouza@gmail.com>
2021-01-02 12:34:34 -0300

    :beetle: codeClimate

--- src/zoo.js
+++ src/zoo.js
@@ -71,7 +71,7 @@ function animalCount(species) {
 
 function entryCalculator(entrants) {
   if (!entrants) return 0;
-  return Object.keys(entrants).reduce((acc, key) => acc + (entrants[key] * prices[key]), 0 );
+  return Object.keys(entrants).reduce((acc, key) => acc + (entrants[key]*prices[key]), 0 );
 }
 
 function animalMap(options) {
