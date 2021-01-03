ac0f340d3932d5c49963b5499a80f4053df2d170
Blucilha <blucilha.nascimento@gmail.com>
2021-01-03 11:43:53 -0300

    Tentando fazer o exercicio 8

--- src/zoo.js
+++ src/zoo.js
@@ -61,7 +61,10 @@ function animalCount(species) {
 }
 
 function entryCalculator(entrants) {
-  // seu código aqui
+  if (entrants === undefined) { return 0 };
+  const { Adult = 0, Child = 0, Senior = 0} = entrants;
+  const contaTotal = (prices.Adult * Adult + prices.Child * Child + prices.Senior * Senior);
+  return contaTotal;
 }
 
 function animalMap(options) {
