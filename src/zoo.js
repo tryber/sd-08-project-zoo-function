28c302a46977a131b85d3f288782b297485d2610
Blucilha <blucilha.nascimento@gmail.com>
2021-01-03 15:50:21 -0300

    Tentando fazer o exercicio 10

--- src/zoo.js
+++ src/zoo.js
@@ -73,7 +73,20 @@ function animalMap(options) {
 }
 
 function schedule(dayName) {
-  // seu código aqui
+  const semana = Object.keys(hours);
+  const previsao = semana.reduce((prev, acc) => {
+    if (acc === 'Monday') {
+      prev[acc] = 'CLOSED';
+      return prev;
+    } else {
+    prev[acc] = `Open from ${hours[acc].open}am until ${hours[acc].close - 12}pm`;
+    return prev;
+    }
+  }, {});
+  if (dayName !== undefined){
+    return { [dayName] : previsao[dayName] };
+  }
+  return previsao;
 }
 
 function oldestFromFirstSpecies(id) {
