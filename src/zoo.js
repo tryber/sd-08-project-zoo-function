5958e384cdbc2c46cd89a9c468f2a3eed8a523f8
WagnerMor <wagnermor@outlook.com>
2021-01-03 04:58:47 -0300

    requisito 06

--- src/zoo.js
+++ src/zoo.js
@@ -40,8 +40,8 @@ function isManager(id) {
   return employees.some((employee, index) => employee.managers[index] === id);
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
 
 function animalCount(species) {
