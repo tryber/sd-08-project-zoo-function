43aa84736112116e4cd433a97c49b59055a263b4
Rafael Greco <rafaelmgreco@hotmail.com>
2021-01-03 12:56:47 -0300

    req 6

--- src/zoo.js
+++ src/zoo.js
@@ -42,8 +42,14 @@ function isManager(id) {
   return manager;
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  data.employees.push({
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  });
 }
 
 function animalCount(species) {
