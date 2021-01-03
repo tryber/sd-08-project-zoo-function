af29cd39df48fc331eb83fbb7421c3148592fdf8
Rafael Greco <rafaelmgreco@hotmail.com>
2021-01-03 12:04:21 -0300

    req 1

--- src/zoo.js
+++ src/zoo.js
@@ -12,7 +12,8 @@ eslint no-unused-vars: [
 const data = require('./data');
 
 function animalsByIds(ids) {
-  // seu código aqui
+  const nome = data.animals.filter(animal => ids.includes(animal.id));
+  return nome;
 }
 
 function animalsOlderThan(animal, age) {
