153476b56712c7c27a9c7f25fb4563ef8f474e53
Rafael <rafaeljavaro750@gmail.com>
2021-01-04 23:42:04 -0300

    Requisito 2 - Verifica se TODOS os animais da especie passada como parametro tem idade maior que o valor passado como segundo parametro

--- src/zoo.js
+++ src/zoo.js
@@ -17,7 +17,9 @@ function animalsByIds(...ids) {
 }
 
 function animalsOlderThan(animal, age) {
-  // seu código aqui
+  return animals
+    .find(species => species.name === animal)
+    .residents.every(resident => resident.age > age);
 }
 
 function employeeByName(employeeName) {
