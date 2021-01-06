54462fbb036bd140af9eed76e5841a4e9518af03
maym4y <jennim.almeida@gmail.com>
2021-01-06 15:41:17 -0300

    createEmployee codeclimateado

--- src/zoo.js
+++ src/zoo.js
@@ -38,7 +38,7 @@ function employeeByName(employeeName) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  const newEmployee = {...personalInfo, ...associatedWith};
+  const newEmployee = { ...personalInfo, ...associatedWith, };
   // newEmployee.managers = associatedWith.managers;
   // newEmployee.responsibleFor = associatedWith.responsibleFor;
   return newEmployee;
