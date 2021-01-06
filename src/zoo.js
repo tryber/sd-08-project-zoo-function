da9fba7fd3430c75e636a5b08cb07e81f6332efb
Jorge William Cardoso Furtado <jorge_william_cardoso@yahoo.com.br>
2021-01-06 15:02:30 -0300

    Fix requesito 8

--- src/zoo.js
+++ src/zoo.js
@@ -9,26 +9,28 @@ eslint no-unused-vars: [
 ]
 */
 
-const { animals, employees, prices } = require('./data');
-const data = require('./data');
+const { animals, employees, prices } = require("./data");
+const data = require("./data");
 
 function animalsByIds(...ids) {
   // seu código aqui
   if (!ids) return [];
-  return animals.filter(animal => ids.find(id => animal.id === id));
+  return animals.filter((animal) => ids.find((id) => animal.id === id));
 }
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
   return animals
-    .find(a => (a.name = animal))
-    .residents.every(resident => resident.age >= age);
+    .find((a) => (a.name = animal))
+    .residents.every((resident) => resident.age >= age);
 }
 
 function employeeByName(employeeName) {
   // seu código aqui
   if (!employeeName) return {};
-  return employees.find(name => name.firstName === employeeName || name.lastName === employeeName);
+  return employees.find(
+    (name) => name.firstName === employeeName || name.lastName === employeeName
+  );
 }
 
 function createEmployee(personalInfo, associatedWith) {
@@ -38,19 +40,24 @@ function createEmployee(personalInfo, associatedWith) {
 
 function isManager(id) {
   // seu código aqui
-  return employees.some(someId => someId.managers.includes(id));
+  return employees.some((someId) => someId.managers.includes(id));
 }
 
-function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+function addEmployee(
+  id,
+  firstName,
+  lastName,
+  managers = [],
+  responsibleFor = []
+) {
   // seu código aqui
-  employees.push(
-    {
-      id,
-      firstName,
-      lastName,
-      managers,
-      responsibleFor,
-    });
+  employees.push({
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  });
 }
 
 function animalCount(species) {
@@ -59,10 +66,9 @@ function animalCount(species) {
     return animals.reduce((acc, animal) => {
       acc[animal.name] = animal.residents.length;
       return acc;
-    },
-      {});
+    }, {});
   }
-  return animals.find(animal => animal.name === species).residents.length;
+  return animals.find((animal) => animal.name === species).residents.length;
 }
 function entryCalculator(entrants) {
   // seu código aqui
@@ -70,11 +76,12 @@ function entryCalculator(entrants) {
     return 0;
   }
   const clientes = Object.keys(entrants);
-  const calculo = clientes.map(individuo => data.prices[individuo]);
+  const calculo = clientes.map((individuo) => data.prices[individuo]);
   const valor = Object.values(entrants);
-  const soma = valor.reduce((acumulador, posAtual, indice) => {
-    return acumulador + (calculo[indice] * posAtual)
-  }, 0)
+  const soma = valor.reduce(
+    (acumulador, posAtual, indice) => acumulador + calculo[indice] * posAtual,
+    0
+  );
   return soma;
 }
 
@@ -88,12 +95,14 @@ function schedule(dayName) {
 
 function oldestFromFirstSpecies(id) {
   // seu código aqui
-  const animalSpecie = animals.find(animal => {
-    return animal.id === employees.find(employee => employee.id === id).responsibleFor[0];
-  });
+  const animalSpecie = animals.find(
+    (animal) =>
+      animal.id ===
+      employees.find((employee) => employee.id === id).responsibleFor[0]
+  );
   let maior = animalSpecie.residents[0].age;
-  animalSpecie.residents.map(element => {
-    if (element.age > maior) return maior = element;
+  animalSpecie.residents.map((element) => {
+    if (element.age > maior) return (maior = element);
   });
   ({ name, sex, age } = maior);
   return [name, sex, age];
@@ -102,7 +111,9 @@ function oldestFromFirstSpecies(id) {
 function increasePrices(percentage) {
   // seu código aqui
   Object.entries(prices).forEach(([key, value]) => {
-    prices[key] = parseFloat(((value + 0.001) * ((percentage / 100) + 1)).toFixed(2));
+    prices[key] = parseFloat(
+      ((value + 0.001) * (percentage / 100 + 1)).toFixed(2)
+    );
   });
 }
 function employeeCoverage(idOrName) {
