78e5397ecf325d417ab2c5bc01c83a05cad1e183
maym4y <jennim.almeida@gmail.com>
2021-01-07 13:53:55 -0300

    addEmployee terminado

--- src/zoo.js
+++ src/zoo.js
@@ -55,7 +55,22 @@ function isManager(id) {
 console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+  if (!responsibleFor) {
+    responsibleFor = [];
+  }
+  if (!managers) {
+    managers = [];
+  }
+  
+  let newEmployee = {
+    id: id,
+    firstName: firstName,
+    lastName: lastName,
+    managers: managers,
+    responsibleFor: responsibleFor
+  };
+  // let n =  data.employees.length;
+  data.employees.push(newEmployee);
 }
 
 function animalCount(species) {
