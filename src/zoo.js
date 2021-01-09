41d72d148a3f59c06a5a9068f9ff80117ca77784
Jorge William Cardoso Furtado <jorge_william_cardoso@yahoo.com.br>
2021-01-09 11:39:00 -0300

    Fix

--- src/zoo.js
+++ src/zoo.js
@@ -104,7 +104,7 @@ function oldestFromFirstSpecies(id) {
 function increasePrices(percentage) {
   // seu código aqui
   Object.entries(prices).forEach(([key, value]) => {
-    prices[key] = parseFloat((((value + 0.001) * percentage) / 100 + 1).toFixed(2));
+    prices[key] = parseFloat((value + 0.001) * (percentage / 100 + 1).toFixed(2));
   });
 }
 
