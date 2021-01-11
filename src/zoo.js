27a64a12ed7f8c3c7cbbe3b0b4ca8c8b5f332c9b
OPedroAzevedo <pvas1996@gmail.com>
2021-01-11 03:09:56 -0300

    Code climate corrections part 4 :z

--- src/zoo.js
+++ src/zoo.js
@@ -14,8 +14,8 @@ function animalsOlderThan(animal, age) {
 function employeeByName(employeeName) {
   return employeeName === undefined
     ? {}
-    : data.employees.find(({ firstName, lastName }) => firstName === employeeName 
-    || lastName === employeeName);
+    : data.employees.find(({ firstName, lastName }) => firstName === employeeName
+      || lastName === employeeName);
 }
 
 function createEmployee(personalInfo, associatedWith) {
@@ -47,7 +47,7 @@ function entryCalculator(entrants) {
     : 0;
 }
 
-function animalMap(optionsv) {
+function animalMap() {
   // seu código aqui
 }
 
