38520f69717d03dea2481670c5bf032216d264c8
OPedroAzevedo <pvas1996@gmail.com>
2021-01-11 02:51:38 -0300

    Code climate corrections

--- src/zoo.js
+++ src/zoo.js
@@ -1,20 +1,20 @@
 const { animals, employees } = require('./data');
 const data = require('./data');
 
-function animalsByIds(... ids) {
+function animalsByIds(...ids) {
   return data.animals.filter(animais => ids.includes(animais.id));
 }
 
 function animalsOlderThan(animal, age) {
-    return animals
-  .find(animais => animais.name === animal)
-  .residents.every(animais => animais.age >= age);
+  return animals
+    .find(animais => animais.name === animal)
+    .residents.every(animais => animais.age >= age);
 }
 
 function employeeByName(employeeName) {
   return employeeName === undefined
     ? {}
-    : data.employees.find(({ firstName, lastName }) => firstName === employeeName || lastName === employeeName,);
+    : data.employees.find(({ firstName, lastName }) => firstName === employeeName || lastName === employeeName);
 }
 
 function createEmployee(personalInfo, associatedWith) {
@@ -26,23 +26,24 @@ function isManager(id) {
 }
 
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
-  data.employees.push({id, firstName, lastName, managers, responsibleFor});
+  data.employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
 
 function animalCount(species) {
   return species
-  ? data.animals.find(animal => animal.name === species).residents.length
-  : data.animals.reduce((counter, obj) => {counter[obj.name] = obj.residents.length;
-  return counter;
-}, {});
+    ? data.animals.find(animal => animal.name === species).residents.length
+    : data.animals.reduce((counter, obj) => {
+      counter[obj.name] = obj.residents.length;
+      return counter;
+    }, {});
 }
 
 function entryCalculator(entrants) {
   return entrants
-  ? Object.keys(entrants).reduce(
-    (counter, current) => counter + (data.prices[current] * entrants[current]), 0,
+    ? Object.keys(entrants).reduce(
+      (counter, current) => counter + (data.prices[current] * entrants[current]), 0,
     )
-  : 0;
+    : 0;
 }
 
 function animalMap(options) {
@@ -63,7 +64,7 @@ function schedule(dayName) {
     return { [dayName]: agenda[dayName] };
   }
   return agenda;
-  }
+}
 
 function oldestFromFirstSpecies(id) {
   const thisResponsibleFor = employees.find(employee => employee.id === id).responsibleFor[0];
