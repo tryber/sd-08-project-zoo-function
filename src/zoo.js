e94eff8031c552ea92f08c8e8a299ec061626f1f
Thais Quintela <thais.s.quintela@gmail.com>
2021-01-11 02:32:07 -0300

    Requisito 1

--- src/zoo.js
+++ src/zoo.js
@@ -9,11 +9,13 @@ eslint no-unused-vars: [
 ]
 */
 
+const { animals } = require('./data');
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
+function animalsByIds(...ids) {
+  return ids.map(id => animals.find(animal => animal.id === id));
 }
+console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
