88b22555d909f5e0b6406b9b9e2261acf25cafd9
MatheuzaoCarvalho <matheusvscarvalho@gmail.com>
2021-01-11 16:01:08 -0300

    Tasks AnimalsByID AnimalsOlderThan - CodeClimate Check

--- src/zoo.js
+++ src/zoo.js
@@ -13,17 +13,25 @@ const { animals } = require('./data');
 
 function animalsByIds(...ids) {
   if (!ids) return [];
-  // return ids.map(id => animals.find(animal => animal.id === id)); Vídeo Plantão e Vídeo PSimões, pra cada id ele vai rodar um map e trazer o animal.
-  return animals.filter(animal => ids.includes(animal.id)); // filtrando o critério (animal) e vendo se ele entra na lista - Plantão 
+// return ids.map(id => animals.find(animal => animal.id === id));
+// Vídeo Plantão e Vídeo PSimões, pra cada id ele vai rodar um map e trazer o animal.
+  return animals.filter(animal => ids.includes(animal.id));
+  // filtrando o critério (animal) e vendo se ele entra na lista - Plantão
 }
 
 function animalsOlderThan(animalName, age) {
   let result = animals.find(animal => animal.name === animalName);
   if (result) result = result.residents.every(animal => animal.age >= age);
-  return result; // Para existir um tratamento de erro, caso não exista o objeto buscado e apresente o erro.
-  //return animals.find(animal => animal.name === animalName).residents.every(animal => animal.age >= age); Buscando a forma de retornar o array do residents, com todas as infos. Assumindo claro que todos os objetos vão retornar, se o resultado for nulo vai dar erro, por isso o tratamento do erro acima. Video PSimões
-}  
-//console.log(animalsOlderThan('otters',7));
+  return result;
+
+  //  Para existir um tratamento de erro, caso não exista o objeto buscado e apresente o erro.
+  //  return animals.find(animal => animal.name === animalName)
+  //  .residents.every(animal => animal.age >= age;
+  //  Buscando a forma de retornar o array do residents, com todas as infos.
+  //  Assumindo claro que todos os objetos vão retornar,
+  //  se o resultado for nulo vai dar erro, por isso o tratamento do erro acima. Video PSimões
+}
+  //  console.log(animalsOlderThan('otters',7));
 
 function employeeByName(employeeName) {
   // seu código aqui
