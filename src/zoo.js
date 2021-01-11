d2da62c5f91a8b8552711ed0eef5b4c0c3cbf7a1
MatheuzaoCarvalho <matheusvscarvalho@gmail.com>
2021-01-11 15:44:00 -0300

    Tasks AnimalsByID AnimalsOlderThan

--- src/zoo.js
+++ src/zoo.js
@@ -9,15 +9,21 @@ eslint no-unused-vars: [
 ]
 */
 
-const data = require('./data');
+const { animals } = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui!
+function animalsByIds(...ids) {
+  if (!ids) return [];
+  // return ids.map(id => animals.find(animal => animal.id === id)); Vídeo Plantão e Vídeo PSimões, pra cada id ele vai rodar um map e trazer o animal.
+  return animals.filter(animal => ids.includes(animal.id)); // filtrando o critério (animal) e vendo se ele entra na lista - Plantão 
 }
 
-function animalsOlderThan(animal, age) {
-  // seu código aqui
-}
+function animalsOlderThan(animalName, age) {
+  let result = animals.find(animal => animal.name === animalName);
+  if (result) result = result.residents.every(animal => animal.age >= age);
+  return result; // Para existir um tratamento de erro, caso não exista o objeto buscado e apresente o erro.
+  //return animals.find(animal => animal.name === animalName).residents.every(animal => animal.age >= age); Buscando a forma de retornar o array do residents, com todas as infos. Assumindo claro que todos os objetos vão retornar, se o resultado for nulo vai dar erro, por isso o tratamento do erro acima. Video PSimões
+}  
+//console.log(animalsOlderThan('otters',7));
 
 function employeeByName(employeeName) {
   // seu código aqui
