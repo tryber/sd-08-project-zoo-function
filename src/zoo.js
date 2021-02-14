d54937ace9172cf49c2443927709de041d73c3f9
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-02-14 06:22:13 -0300

    corrigindo issues

--- src/zoo.js
+++ src/zoo.js
@@ -20,42 +20,42 @@ function animalsByIds(...ids) {
 }
 
 function animalsOlderThan(animal, age) {
-  return animals.find(cur => cur.name === animal).residents.every(cur => cur.age > age);
+  // return animals.find(cur => cur.name === animal).residents.every(cur => cur.age > age);
 }
 
 function employeeByName(employeeName) {
-  if (employeeName === undefined) {
-    return {};
-  }
-  return employees.find(cur => employeeName === cur.firstName || employeeName === cur.lastName);
+  // if (employeeName === undefined) {
+  //   return {};
+  // }
+  // return employees.find(cur => employeeName === cur.firstName || employeeName === cur.lastName);
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  return { ...personalInfo, ...associatedWith, }
+  // return { ...personalInfo, ...associatedWith, }
 }
 
 function isManager(id) {
-  return employees.some(cur => cur.managers.find(cur => cur === id));
+  // return employees.some(cur => cur.managers.find(cur => cur === id));
 }
 
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
-  employees.push({ id, firstName, lastName, managers, responsibleFor });
+  // employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
 
 function animalCount(species) {
-  if (species === undefined) {
-    const lista = {};
-    animals.forEach(cur => lista[cur.name] = cur.residents.length);
-    return lista;
-  }
-  return animals.find(cur => cur.name === species).residents.length;
+  // if (species === undefined) {
+  //   const lista = {};
+  //   animals.forEach(cur => lista[cur.name] = cur.residents.length);
+  //   return lista;
+  // }
+  // return animals.find(cur => cur.name === species).residents.length;
 }
 
 function entryCalculator(entrants) {
-  if (entrants === undefined || Object.keys(entrants).length === 0) {
-    return 0;
-  }
-  return Object.keys(entrants).reduce((acc, cur) => acc + entrants[cur] * prices[cur], 0);
+  // if (entrants === undefined || Object.keys(entrants).length === 0) {
+  //   return 0;
+  // }
+  // return Object.keys(entrants).reduce((acc, cur) => acc + entrants[cur] * prices[cur], 0);
 }
 
 function animalMap(options = 0) {
@@ -193,60 +193,60 @@ function animalMap(options = 0) {
 }
 
 function schedule(dayName) {
-  const cronograma = {
-    Tuesday: 'Open from 8am until 6pm',
-    Wednesday: 'Open from 8am until 6pm',
-    Thursday: 'Open from 10am until 8pm',
-    Friday: 'Open from 10am until 8pm',
-    Saturday: 'Open from 8am until 10pm',
-    Sunday: 'Open from 8am until 8pm',
-    Monday: 'CLOSED',
-  }
-  if (dayName === undefined) {
-    return cronograma;
-  }
-  return {
-    [dayName]: cronograma[dayName]
-  };
+  // const cronograma = {
+  //   Tuesday: 'Open from 8am until 6pm',
+  //   Wednesday: 'Open from 8am until 6pm',
+  //   Thursday: 'Open from 10am until 8pm',
+  //   Friday: 'Open from 10am until 8pm',
+  //   Saturday: 'Open from 8am until 10pm',
+  //   Sunday: 'Open from 8am until 8pm',
+  //   Monday: 'CLOSED',
+  // }
+  // if (dayName === undefined) {
+  //   return cronograma;
+  // }
+  // return {
+  //   [dayName]: cronograma[dayName]
+  // };
 }
 
 function oldestFromFirstSpecies(id) {
-  const idDaEspecie = employees.find(cur => cur.id === id).responsibleFor[0];
-  const animais = animals.find(cur => cur.id === idDaEspecie).residents;
-  const maisVelho = animais.sort((a, b) => b.age - a.age)[0];
-  return Object.values(maisVelho);
+  // const idDaEspecie = employees.find(cur => cur.id === id).responsibleFor[0];
+  // const animais = animals.find(cur => cur.id === idDaEspecie).residents;
+  // const maisVelho = animais.sort((a, b) => b.age - a.age)[0];
+  // return Object.values(maisVelho);
 }
 
 function increasePrices(percentage) {
-  const adulto = parseFloat((prices.Adult + percentage / 100 * prices.Adult + 0.005).toFixed(2));
-  prices.Adult = adulto;
-  const idoso = parseFloat((prices.Senior + percentage / 100 * prices.Senior + 0.005).toFixed(2));
-  prices.Senior = idoso;
-  const criança = parseFloat((prices.Child + percentage / 100 * prices.Child + 0.005).toFixed(2));
-  prices.Child = criança;
-  return prices;
+  // const adulto = parseFloat((prices.Adult + percentage / 100 * prices.Adult + 0.005).toFixed(2));
+  // prices.Adult = adulto;
+  // const idoso = parseFloat((prices.Senior + percentage / 100 * prices.Senior + 0.005).toFixed(2));
+  // prices.Senior = idoso;
+  // const criança = parseFloat((prices.Child + percentage / 100 * prices.Child + 0.005).toFixed(2));
+  // prices.Child = criança;
+  // return prices;
 }
 
 function employeeCoverage(idOrName) {
-  if (idOrName === undefined) {
-    const lista = {};
-    employees.forEach(employee => lista[`${employee.firstName} ${employee.lastName}`] = employee.responsibleFor.map(speciesId => {
-      const especie = animals.find(especie => especie.id === speciesId).name;
-      return especie;
-    }))
-    return lista;
-  } else {
-    const employee = employees
-    .find(employee => employee.id === idOrName ||
-      employee.firstName === idOrName || employee.lastName === idOrName);
-    return {
-      [`${employee.firstName} ${employee.lastName}`]: employee.responsibleFor
-      .map(speciesId => {
-        const especie = animals.find(especie => especie.id === speciesId).name;
-        return especie;
-      }),
-    };
-  }
+  // if (idOrName === undefined) {
+  //   const lista = {};
+  //   employees.forEach(employee => lista[`${employee.firstName} ${employee.lastName}`] = employee.responsibleFor.map(speciesId => {
+  //     const especie = animals.find(especie => especie.id === speciesId).name;
+  //     return especie;
+  //   }))
+  //   return lista;
+  // } else {
+  //   const employee = employees
+  //   .find(employee => employee.id === idOrName ||
+  //     employee.firstName === idOrName || employee.lastName === idOrName);
+  //   return {
+  //     [`${employee.firstName} ${employee.lastName}`]: employee.responsibleFor
+  //     .map(speciesId => {
+  //       const especie = animals.find(especie => especie.id === speciesId).name;
+  //       return especie;
+  //     }),
+  //   };
+  // }
 }
 
 module.exports = {
