94601b5aac38806316bb1fb8ed882ce4fde39ffa
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-02-14 01:26:08 -0300

    requisito 12

--- src/zoo.js
+++ src/zoo.js
@@ -234,7 +234,7 @@ function schedule(dayName) {
     Saturday: 'Opem from 8am until 10pm',
     Sunday: 'Opem from 8am until 8pm',
     Monday: 'CLOSED',
-  };
+  }
   if (dayName === undefined) {
     return cronograma;
   }
@@ -251,7 +251,13 @@ function oldestFromFirstSpecies(id) {
 }
 
 function increasePrices(percentage) {
-  // seu código aqui
+  const adulto = parseFloat((prices.Adult + percentage / 100 * prices.Adult + 0.005).toFixed(2));
+  prices.Adult = adulto;
+  const idoso = parseFloat((prices.Senior + percentage / 100 * prices.Senior + 0.005).toFixed(2));
+  prices.Senior = idoso;
+  const criança = parseFloat((prices.Child + percentage / 100 * prices.Child + 0.005).toFixed(2));
+  prices.Child = criança;
+  return prices;
 }
 
 function employeeCoverage(idOrName) {
